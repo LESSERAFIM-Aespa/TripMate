fd69094e1709b46231c3522d4444d7ab98ed2b7b
seongssu <loveforggong@gmail.com>
2023-10-11 02:37:35 +0900

    gourmet 뷰

--- app/src/main/java/kr/sparta/tripmate/fragment/gourmet/GourmetAdapter.kt
+++ app/src/main/java/kr/sparta/tripmate/fragment/gourmet/GourmetAdapter.kt
@@ -1,4 +1,45 @@
 package kr.sparta.tripmate.fragment.gourmet
 
-class GourmetAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kr.sparta.tripmate.api.model.GourmetModel
+import kr.sparta.tripmate.databinding.GourmetitemsBinding
+
+
+class GourmetAdapter(private val context: Context) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var items = ArrayList<GourmetModel>()
+
+    inner class GourmetViewHolder(private val binding: GourmetitemsBinding) : RecyclerView
+    .ViewHolder(binding.root) {
+        fun bind(items: GourmetModel) {
+            binding.apply {
+                gourmetTitle.text = items.title
+                Glide.with(context).load(items.url).into(gourmetUrl)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = GourmetitemsBinding.inflate(
+            LayoutInflater.from(parent.context), parent,
+            false
+        )
+        return GourmetViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder as GourmetViewHolder
+        val items = items[position]
+        holder.bind(items)
+    }
+
 }
--- app/src/main/java/kr/sparta/tripmate/fragment/gourmet/GourmetFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/fragment/gourmet/GourmetFragment.kt
@@ -1,18 +1,95 @@
 package kr.sparta.tripmate.fragment.gourmet
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import kr.sparta.tripmate.R
+import kr.sparta.tripmate.api.NaverNetWorkClient
+import kr.sparta.tripmate.api.NaverNetWorkClient.apiService
+import kr.sparta.tripmate.databinding.FragmentGourmetBinding
+import kr.sparta.tripmate.`interface`.ItemClick
+import kr.sparta.tripmate.viewmodel.gourmet.GourmetFactory
+import kr.sparta.tripmate.viewmodel.gourmet.GourmetViewModel
 
 class GourmetFragment : Fragment() {
+    private val binding by lazy { FragmentGourmetBinding.inflate(layoutInflater) }
+    private lateinit var gourmetAdapter: GourmetAdapter
+    private val apiServiceInstance = NaverNetWorkClient.apiService
+    private val gourmetViewModel : GourmetViewModel by viewModels {GourmetFactory(apiServiceInstance)}
+
+    var searchQuery:String? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gourmet, container, false)
+
+        setUpView()
+        searchView()
+
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        gourmetViewModel.apply {
+            gourResult.observe(viewLifecycleOwner){
+                gourmetAdapter.items.clear()
+                gourmetAdapter.items.addAll(it)
+                gourmetAdapter.notifyDataSetChanged()
+                isLoading.observe(viewLifecycleOwner){isLoading ->
+                    binding.gourmetLoading.visibility = if(isLoading) View.VISIBLE else View.GONE
+                }
+
+            }
+        }
+    }
+    private fun searchView() {
+        gourmetAdapter = GourmetAdapter(requireContext())
+
+        binding.gourmetRecyclerView.apply {
+            layoutManager = GridLayoutManager(context,3)
+            adapter = gourmetAdapter
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setUpView() {
+        binding.gourmetSearchView.apply {
+            isSubmitButtonEnabled = true
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if(!query.isNullOrEmpty()){
+                        searchQuery = query.trim()
+                        Log.d("TripMates", "검색어 : ${searchQuery}")
+                        setupListeners()
+                    }
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+
+            })
+        }
+    }
+    private fun setupListeners(){
+        searchQuery?.let {
+            gourmetViewModel.GourmetServerResults(searchQuery!!)
+        }
     }
 }
