86aeea520bad9305ffb27d7c9e0ab2cb46e7b938
SeoJungHan <seojh5939@gmail.com>
2023-10-13 18:21:08 +0900

    [Feat] : Domain Layer 모델 정의

---
+++ app/src/main/java/kr/sparta/tripmate/domain/model/ScrapMapper.kt
@@ -0,0 +1,38 @@
+package kr.sparta.tripmate.domain.model
+
+import kr.sparta.tripmate.data.model.ScrapItems
+import kr.sparta.tripmate.data.model.ScrapServerData
+
+fun ScrapServerData.toEntity(): ScrapServerDataEntity = ScrapServerDataEntity(
+    lastBuildDate = lastBuildDate,
+    total = total,
+    start = start,
+    display = display,
+    items = items?.toEntity()
+)
+
+fun ScrapItems.toEntity(): ScrapItemsEntity = ScrapItemsEntity(
+    title = title,
+    link = link,
+    description = description,
+    bloggername = bloggername,
+    bloggerlink = bloggerlink,
+    postdate = postdate
+)
+
+fun ArrayList<ScrapItems>.toEntity(): ArrayList<ScrapItemsEntity> {
+    val list = ArrayList<ScrapItemsEntity>()
+    for (i in this.indices) {
+        list.add(this[i].toEntity())
+    }
+    return list
+}
+
+fun ScrapItemsEntity.toScrapModel(): ScrapModel = ScrapModel(
+    title = title.orEmpty(),
+    url = link.orEmpty(),
+    description = description.orEmpty(),
+    bloggername = bloggername.orEmpty(),
+    bloggerlink = bloggerlink.orEmpty(),
+    postdate = postdate.orEmpty(),
+)
---
+++ app/src/main/java/kr/sparta/tripmate/domain/model/ScrapModel.kt
@@ -0,0 +1,14 @@
+package kr.sparta.tripmate.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ScrapModel(
+    val title: String,
+    val url: String,
+    val description: String,
+    val bloggername: String,
+    val bloggerlink: String,
+    val postdate: String,
+) : Parcelable
