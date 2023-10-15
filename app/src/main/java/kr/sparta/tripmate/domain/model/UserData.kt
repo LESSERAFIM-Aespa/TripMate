6baaf447e26f30d85368e6cb0049a9825670ec82
seongssu <loveforggong@gmail.com>
2023-10-16 06:06:37 +0900

    저장 UserData

---
+++ app/src/main/java/kr/sparta/tripmate/domain/model/UserData.kt
@@ -0,0 +1,8 @@
+package kr.sparta.tripmate.domain.model
+
+data class UserData(
+    var login_Id: String,
+    var login_NickName:String,
+    var login_profile:String,
+    var login_Uid:String
+)
