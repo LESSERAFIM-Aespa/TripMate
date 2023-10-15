5c3b34db826caa7d2fbfa5ddd151ea5bed20c5c8
youneeo <steamdacut@gmail.com>
2023-10-15 11:54:33 +0900

    [Design] 게시판 글쓰기,작성 페이지 xml 파일 코드정리

--- app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
@@ -1,12 +1,22 @@
 package kr.sparta.tripmate.ui.community
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.sparta.tripmate.R
+import kr.sparta.tripmate.databinding.ActivityCommunityWriteBinding
 
 class CommunityWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCommunityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community_write)
+
+        binding = ActivityCommunityWriteBinding.inflate(layoutInflater)
+
+        binding.communityWriteTitle.setOnClickListener {
+            val intent = Intent()
+        }
+
     }
 }
