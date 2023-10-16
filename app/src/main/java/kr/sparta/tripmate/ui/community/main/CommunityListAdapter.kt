ea8d734ddb67dae9f390be4df3f379f009319a89
SeoJungHan <seojh5939@gmail.com>
2023-10-16 20:40:12 +0900

    [Feat] : CommunityMain 페이지 Recyclerview 적용완료

---
+++ app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityListAdapter.kt
@@ -0,0 +1,43 @@
+package kr.sparta.tripmate.ui.community.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
+import kr.sparta.tripmate.databinding.FragmentCommunityMainItemBinding
+
+class CommunityListAdapter: ListAdapter<CommunityModel, CommunityListAdapter.CommunityHolder>(
+    object: DiffUtil.ItemCallback<CommunityModel>() {
+        override fun areItemsTheSame(oldItem: CommunityModel, newItem: CommunityModel): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: CommunityModel, newItem: CommunityModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommunityHolder {
+        val view = FragmentCommunityMainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CommunityHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CommunityHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class CommunityHolder(private val binding : FragmentCommunityMainItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CommunityModel)=with(binding) {
+            communityMainThumbnail.load(item.thumbnail)
+            communityMainTitle.text = item.title
+            communityMainProfileNickname.text = item.profileNickname
+            communityMainProfileThumbnail.load(item.profileThumbnail)
+            communityMainViews.text = item.views
+            communityMainLikes.text = item.likes
+        }
+    }
+}
