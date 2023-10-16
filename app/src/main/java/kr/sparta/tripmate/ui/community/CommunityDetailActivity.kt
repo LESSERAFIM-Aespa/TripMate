9ff3176e6c999d677f337c4f100890daaf02d32c
youneeo <steamdacut@gmail.com>
2023-10-16 20:42:56 +0900

    [feat] 커뮤니티 글작성,디테일 페이지 간 화면전환

--- app/src/main/java/kr/sparta/tripmate/ui/community/CommunityDetailActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/CommunityDetailActivity.kt
@@ -1,12 +1,22 @@
 package kr.sparta.tripmate.ui.community
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.sparta.tripmate.R
+import kr.sparta.tripmate.databinding.ActivityCommunityDetailBinding
 
 class CommunityDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityCommunityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community_detail)
+        binding = ActivityCommunityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.communityDetailBackbutton.setOnClickListener {
+            val intent = Intent(this,CommunityWriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
