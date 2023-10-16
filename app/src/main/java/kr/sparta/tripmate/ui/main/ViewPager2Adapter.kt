57f146f24cdc41f3a5cfc62ef08d354ec4fdeeae
SeoJungHan <seojh5939@gmail.com>
2023-10-16 20:39:50 +0900

    [Refactor] : 폴더이동 , import 변경적용

--- app/src/main/java/kr/sparta/tripmate/ui/main/ViewPager2Adapter.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/main/ViewPager2Adapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import kr.sparta.tripmate.ui.budget.BudgetFragment
-import kr.sparta.tripmate.ui.community.CommunityFragment
+import kr.sparta.tripmate.ui.community.main.CommunityFragment
 import kr.sparta.tripmate.ui.scrap.ScrapFragment
 import kr.sparta.tripmate.ui.home.HomeFragment
 import kr.sparta.tripmate.ui.mypage.home.MyPageFragment
