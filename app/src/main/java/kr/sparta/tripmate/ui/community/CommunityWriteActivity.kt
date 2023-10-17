ac1387e13ef709d7c70fb92265a6c3954671ab34
youneeo <steamdacut@gmail.com>
2023-10-18 04:03:04 +0900

    [feat] 커뮤니티 게시판에서 글 작성시 리싸이클러뷰에 표시 되게 구현

--- app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
@@ -1,20 +1,61 @@
 package kr.sparta.tripmate.ui.community
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import kr.sparta.tripmate.R
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kr.sparta.tripmate.databinding.ActivityCommunityWriteBinding
-import kr.sparta.tripmate.ui.main.MainActivity
+import kr.sparta.tripmate.ui.community.main.CommunityListAdapter
+import kr.sparta.tripmate.ui.community.main.CommunityModel
+import kr.sparta.tripmate.ui.viewmodel.community.CommunityViewModel
 
 class CommunityWriteActivity : AppCompatActivity() {
 
+    private val viewModel: CommunityViewModel by viewModels()
+
     private lateinit var binding: ActivityCommunityWriteBinding
+
+    val dataModelList = mutableListOf<CommunityModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val newData = mutableListOf<CommunityModel>()// newData에 데이터를 추가
+        val adapter = CommunityListAdapter(dataModelList)
+        val database = Firebase.database
+        val myRef = database.getReference("CommunityData")
+
+
+        viewModel.updateDataModelList(newData) // ViewModel을 통해 데이터를 업데이트
+
+
+//        myRef.child(Firebase.auth.currentUser!!.uid)
+//            .addValueEventListener(object : ValueEventListener{
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    dataModelList.clear() // 기존 데이터를 지우고 새 데이터로 교체
+//
+//                    for (dataModel in snapshot.children){
+//                        Log.d("Data",dataModel.toString())
+//                        dataModelList.add(dataModel.getValue(CommunityModel::class.java)!!)
+//                    }
+//                    Log.d("DataModel", dataModelList.toString())
+//
+//                    adapter.notifyDataSetChanged()
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//            })
+
         binding = ActivityCommunityWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -22,9 +63,32 @@ class CommunityWriteActivity : AppCompatActivity() {
             finish()
         }
         binding.communityWriteShare.setOnClickListener {
-            val intent = Intent(this, CommunityDetailActivity::class.java)
-            startActivity(intent)
 
+            //            파이어베이스로 데이터 저장하기
+            val body_write = binding.communityWriteDescription.text.toString()
+            val title_write = binding.communityWriteTitle.text.toString()
+
+            val database = Firebase.database
+            val myRef = database.getReference("CommunityData").child(Firebase.auth.currentUser!!.uid)
+            val model = CommunityModel(
+                id = "게시글 ID",
+                thumbnail = null, // null 또는 이미지 URL
+                title = title_write, // title_write 변수의 값
+                body = body_write, // body_write 변수의 값
+                profileNickname = "프로필 닉네임",
+                profileThumbnail = null, // null 또는 프로필 이미지 URL
+                views = "조회수",
+                likes = "좋아요 수"
+            )
+            myRef
+                .push()
+                .setValue(model)
+                .addOnSuccessListener {
+                    // 데이터가 성공적으로 저장될 때 RecyclerView 업데이트
+                    val newData = mutableListOf<CommunityModel>() // 새 데이터를 가져오는 코드가 필요
+                    viewModel.updateDataModelList(newData) // ViewModel을 통해 데이터를 업데이트
+                }
+            Toast.makeText(this,"글이 게시되었습니다(저장완료)",Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
@@ -1,11 +1,15 @@
 package kr.sparta.tripmate.ui.community.main
 
 data class CommunityModel(
-    val id : Double, // 게시글 id
+    val id: String, // 게시글 id
     val thumbnail: String?, // 이미지
-    val title : String?, // 제목
+    val title: String?, // 제목
+    val body: String?, // 게시글 내용
     val profileNickname: String?, // 프로필 닉네임
     val profileThumbnail: String?, // 프로필 이미지
     val views: String?, // 조회수
-    val likes: String?, // 좋아요 수
-)
+    val likes: String? // 좋아요 수
+) {
+    // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
+    constructor() : this("", null, null, null, null, null, null, null)
+}
