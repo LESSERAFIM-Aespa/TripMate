5c955430c6166274e8009be10cf8c530068718ad
PotatoMeme <kshk0897@gmail.com>
2023-10-17 16:00:46 +0900

    [Design] BudgetDetailFragment -> BudgetDetailActivity

---
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/BudgetDetailActivity.kt
@@ -0,0 +1,12 @@
+package kr.sparta.tripmate.ui.budget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.sparta.tripmate.R
+
+class BudgetDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_budget_detail)
+    }
+}
