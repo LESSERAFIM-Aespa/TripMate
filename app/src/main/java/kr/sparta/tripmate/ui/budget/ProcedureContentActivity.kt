adefc1c1432906e87447d0d2dcc241de3b310143
PotatoMeme <kshk0897@gmail.com>
2023-10-17 18:43:53 +0900

    [Design] 가계부_세부_컨탠츠(ProcedureContent)액티비티 추가

---
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/ProcedureContentActivity.kt
@@ -0,0 +1,12 @@
+package kr.sparta.tripmate.ui.budget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.sparta.tripmate.R
+
+class ProcedureContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_procedure_content)
+    }
+}
