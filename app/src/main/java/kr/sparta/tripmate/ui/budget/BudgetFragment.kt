a015dbb0556b733b3d4db026000ef8eec4997c3f
PotatoMeme <kshk0897@gmail.com>
2023-10-18 20:47:04 +0900

    [Feat] 가계부리스트 ViewModel및 Adapter적용

--- app/src/main/java/kr/sparta/tripmate/ui/budget/BudgetFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/BudgetFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.sparta.tripmate.R
+import kr.sparta.tripmate.data.repository.BudgetRepositoryImpl
 import kr.sparta.tripmate.databinding.FragmentBudgetBinding
+import kr.sparta.tripmate.domain.repository.BudgetRepository
+import kr.sparta.tripmate.ui.viewmodel.budget.BudgetViewModel
+import kr.sparta.tripmate.ui.viewmodel.budget.BudgetViewModelFactory
 
 class BudgetFragment : Fragment() {
     companion object {
@@ -17,6 +23,24 @@ class BudgetFragment : Fragment() {
     private var _binding: FragmentBudgetBinding? = null
     private val binding: FragmentBudgetBinding
         get() = _binding!!
+
+    private val budgetAdapter by lazy {
+        BudgetAdapter(object : BudgetAdapter.BudgetListEventListener {
+            override fun itemClicked(num: Int) {
+                //startActivity(BudgetDetailActivity.newIntent(this@BudgetFragment.requireContext(),num))
+                startActivity(
+                    BudgetContentActivity.newIntentForEdit(
+                        this@BudgetFragment.requireContext(),
+                        num
+                    )
+                )
+            }
+        })
+    }
+
+    private val budgetViewModel: BudgetViewModel by viewModels {
+        BudgetViewModelFactory(BudgetRepositoryImpl(requireContext()))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,19 +54,38 @@ class BudgetFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        initViewModels()
     }
+
 
     private fun initViews() = with(binding) {
         budgetListFloatingactionbutton.setOnClickListener {
             startActivity(BudgetContentActivity.newIntentForAdd(requireContext()))
         }
 
+        budgetListRecyclerview.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = budgetAdapter
+        }
+
         //test
         // 이동연결확립후 삭제할거
         textView.setOnClickListener {
-            startActivity(BudgetDetailActivity.newIntent(requireContext(),1))
+            startActivity(BudgetDetailActivity.newIntent(requireContext(), 1))
         }
     }
+
+    private fun initViewModels() {
+        with(budgetViewModel) {
+            budgetLiveDataWhenBudgetChanged.observe(viewLifecycleOwner) {
+                budgetAdapter.submitList(it)
+            }
+            budgetLiveDataWhenProcedureChanged.observe(viewLifecycleOwner) {
+                budgetAdapter.submitList(it)
+            }
+        }
+    }
+
 
     override fun onDestroy() {
         _binding = null
--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/BudgetViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/BudgetViewModel.kt
@@ -1,4 +1,14 @@
 package kr.sparta.tripmate.ui.viewmodel.budget
 
-class BudgetViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import kr.sparta.tripmate.data.model.budget.Budget
+import kr.sparta.tripmate.domain.repository.BudgetRepository
+
+class BudgetViewModel(repository: BudgetRepository) : ViewModel() {
+    val budgetLiveDataWhenBudgetChanged: LiveData<List<Budget>> =
+        repository.getAllBudgetsToFlowWhenBugetsChanged().asLiveData()
+    val budgetLiveDataWhenProcedureChanged: LiveData<List<Budget>> =
+        repository.getAllBugetsToFlowWhenProceduresChanged().asLiveData()
 }
