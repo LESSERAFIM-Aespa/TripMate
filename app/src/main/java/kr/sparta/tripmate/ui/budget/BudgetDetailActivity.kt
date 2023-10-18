66d493c459771df77b1e09d3d095ce7cbf057f64
SeoJungHan <seojh5939@gmail.com>
2023-10-18 21:17:34 +0900

    [Feat] : 가계부 세부과정 Activity ViewPager+TabLayout+RecyclerView 생성

--- app/src/main/java/kr/sparta/tripmate/ui/budget/BudgetDetailActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/BudgetDetailActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.sparta.tripmate.R
+import com.google.android.material.tabs.TabLayoutMediator
 import kr.sparta.tripmate.databinding.ActivityBudgetDetailBinding
+import kr.sparta.tripmate.ui.budget.detail.main.BudgetDetailViewPagerAdapter
 
+/**
+ * 작성자: 서정한
+ * 내용: 가계부 세부과정 Activity
+ * */
 class BudgetDetailActivity : AppCompatActivity() {
 
     companion object {
@@ -26,6 +31,10 @@ class BudgetDetailActivity : AppCompatActivity() {
         ActivityBudgetDetailBinding.inflate(layoutInflater)
     }
 
+    private val adapter by lazy {
+        BudgetDetailViewPagerAdapter(this@BudgetDetailActivity)
+    }
+
     private val budgetNum by lazy {
         intent.getIntExtra(EXTRA_BUDGET_NUM, 0)
     }
@@ -38,21 +47,37 @@ class BudgetDetailActivity : AppCompatActivity() {
     }
 
     private fun initViews() = with(binding) {
+        // TabLayout x ViewPager2
+        budgetDetailViewpager.adapter = adapter
+        TabLayoutMediator(budgetDetailTablayout, budgetDetailViewpager) { tab, position ->
+            tab.setText(adapter.getTitle(position))
+        }.attach()
+
+        // 추가하기 FAB
         budgetDetailFloatingactionbutton.setOnClickListener {
             startActivity(ProcedureContentActivity.newIntentForAdd(this@BudgetDetailActivity))
         }
 
+        // 수정 아이콘
         budgetDetailEditImageview.setOnClickListener {
-            startActivity(BudgetContentActivity.newIntentForEdit(this@BudgetDetailActivity,budgetNum))
+            startActivity(
+                BudgetContentActivity.newIntentForEdit(
+                    this@BudgetDetailActivity,
+                    budgetNum
+                )
+            )
         }
 
+        // 삭제 아이콘
         budgetDetailDeleteImageview.setOnClickListener {
             showDeleteBudgetDialog()
         }
 
-        budgetDetailBackImageview.setOnClickListener {
+        // 뒤로가기
+        budgetDetailToolbar.setNavigationOnClickListener {
             finish()
         }
+
     }
 
     private fun showDeleteBudgetDialog() {
