e548c7cc24b246064ca2fbd9a09dfe178e5bf7f9
PotatoMeme <kshk0897@gmail.com>
2023-10-18 16:53:45 +0900

    [Feat] 가계부 insert 기능 추가

---
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/BudgetContentViewModelFactory.kt
@@ -0,0 +1,20 @@
+package kr.sparta.tripmate.ui.viewmodel.budget
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.sparta.tripmate.domain.repository.BudgetRepository
+import kr.sparta.tripmate.ui.budget.BudgetContentType
+
+
+class BudgetContentViewModelFactory(
+    private val entryType: BudgetContentType,
+    private val repository: BudgetRepository,
+    private val budgetNum: Int = 0,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BudgetContentViewModel::class.java)) {
+            return BudgetContentViewModel(entryType, repository, budgetNum) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
