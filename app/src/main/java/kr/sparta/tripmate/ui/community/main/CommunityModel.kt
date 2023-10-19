14029a56ad1340081ef6557d8d3bfef94b6ce948
youneeo <steamdacut@gmail.com>
2023-10-20 01:55:12 +0900

    [feat] 커뮤니티 메인페이지 섬네일 클릭시 디테일페이지 연결

--- app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
@@ -16,6 +16,6 @@ data class CommunityModel(
     val key : String?,
     val commuIsLike:Boolean = false
 ) : Parcelable{
-        // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
-        constructor() : this("", null, null, null, null, null, null, null, null)
+    // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
+    constructor() : this("", null, null, null, null, null, null, null, null)
 }
