bc7a73ea8b533e527d6fd4119aff6ec3b7583527
SeoJungHan <seojh5939@gmail.com>
2023-10-19 22:00:57 +0900

    [Refector] : ScrapModel -> ScrapEntity 변경

--- app/src/main/java/kr/sparta/tripmate/data/repository/home/HomeFirstRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/home/HomeFirstRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kr.sparta.tripmate.data.model.scrap.ScrapModel
 <<<<<<< HEAD
+<<<<<<< HEAD
 import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
 
 class HomeFirstRepositoryImpl() {
@@ -31,12 +32,15 @@ class HomeFirstRepositoryImpl() {
                     firstData.value = mutableListOf()
                 }
 =======
+=======
+import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
+>>>>>>> 31e02f9 ([Refector] : ScrapModel -> ScrapEntity 변경)
 
 
 class HomeFirstRepositoryImpl() {
     private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
-    fun getData(uid:String): LiveData<MutableList<ScrapModel>> {
-        val firstData = MutableLiveData<MutableList<ScrapModel>>()
+    fun getData(uid:String): LiveData<MutableList<ScrapEntity>> {
+        val firstData = MutableLiveData<MutableList<ScrapEntity>>()
 
         val dataListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -53,9 +57,9 @@ class HomeFirstRepositoryImpl() {
 =======
                 try{
                     if (snapshot.exists()) {
-                        val homeScrapDataList = mutableListOf<ScrapModel>()
+                        val homeScrapDataList = mutableListOf<ScrapEntity>()
                         for (dataSnapshot in snapshot.children) {
-                            val homeScrapData = dataSnapshot.getValue(ScrapModel::class.java)
+                            val homeScrapData = dataSnapshot.getValue(ScrapEntity::class.java)
                             homeScrapData?.let { homeScrapDataList.add(it) }
                         }
                         firstData.value = homeScrapDataList
--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/FirstViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/FirstViewModel.kt
@@ -5,18 +5,26 @@ import androidx.lifecycle.ViewModel
 import kr.sparta.tripmate.data.repository.home.HomeFirstRepositoryImpl
 import kr.sparta.tripmate.data.model.scrap.ScrapModel
 <<<<<<< HEAD
+<<<<<<< HEAD
 import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
 =======
 >>>>>>> c8d8871 ([hotfix] 에러 수정 및 리펠토링)
+=======
+import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
+>>>>>>> a4d949b ([Refector] : ScrapModel -> ScrapEntity 변경)
 
 class FirstViewModel() : ViewModel() {
     private val homeRepository = HomeFirstRepositoryImpl()
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     fun getFirstData(uid:String): LiveData<MutableList<ScrapEntity>> {
 =======
     fun getFirstData(uid:String): LiveData<MutableList<ScrapModel>> {
 >>>>>>> c8d8871 ([hotfix] 에러 수정 및 리펠토링)
+=======
+    fun getFirstData(uid:String): LiveData<MutableList<ScrapEntity>> {
+>>>>>>> a4d949b ([Refector] : ScrapModel -> ScrapEntity 변경)
         return homeRepository.getData(uid)
     }
 }
