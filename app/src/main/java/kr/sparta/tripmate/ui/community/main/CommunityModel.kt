263cfc70e1f3ff4bcb8cc1e530c0f46c84d0cd94
youneeo <steamdacut@gmail.com>
2023-10-20 01:55:12 +0900

    [feat] 커뮤니티 메인페이지 섬네일 클릭시 디테일페이지 연결

--- app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
@@ -1,5 +1,9 @@
 package kr.sparta.tripmate.ui.community.main
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CommunityModel(
     val id: String, // 게시글 id
     val thumbnail: String?, // 이미지
@@ -9,7 +13,7 @@ data class CommunityModel(
     val profileThumbnail: String?, // 프로필 이미지
     val views: String?, // 조회수
     val likes: String? // 좋아요 수
-) {
+) : Parcelable{
     // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
     constructor() : this("", null, null, null, null, null, null, null)
 }
