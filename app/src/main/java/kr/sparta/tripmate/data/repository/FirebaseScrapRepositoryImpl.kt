dfa16ef2e206b8247290d71ad4894568bced3a1d
SeoJungHan <seojh5939@gmail.com>
2023-10-20 12:33:23 +0900

    [Refector] : 이름변경 & 코드정리

--- app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseScrapRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseScrapRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package kr.sparta.tripmate.data.repository
 
 import androidx.lifecycle.MutableLiveData
-import kr.sparta.tripmate.data.datasource.remote.GetFirebaseDatabaseRemoteSource
+import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
 import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
 import kr.sparta.tripmate.domain.repository.FirebaseScrapRepository
 
-class FirebaseScrapRepositoryImpl(private val remoteSource: GetFirebaseDatabaseRemoteSource) : FirebaseScrapRepository {
+class FirebaseScrapRepositoryImpl(private val remoteSource: FirebaseDBRemoteDataSource) : FirebaseScrapRepository {
     override fun getScrapedData(uid: String, liveData: MutableLiveData<List<ScrapEntity>>) = remoteSource.getScrapedData(uid, liveData)
 }
--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/mypage/BookmarkPageFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/mypage/BookmarkPageFactory.kt
@@ -2,14 +2,14 @@ package kr.sparta.tripmate.ui.viewmodel.mypage
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.data.datasource.remote.GetFirebaseDatabaseRemoteSource
+import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
 import kr.sparta.tripmate.data.repository.FirebaseScrapRepositoryImpl
 import kr.sparta.tripmate.domain.repository.FirebaseScrapRepository
 import kr.sparta.tripmate.domain.usecase.GetFirebaseScrapData
 
 class BookmarkPageFactory : ViewModelProvider.Factory {
     private val repository: FirebaseScrapRepository by lazy {
-        FirebaseScrapRepositoryImpl(GetFirebaseDatabaseRemoteSource())
+        FirebaseScrapRepositoryImpl(FirebaseDBRemoteDataSource())
     }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
