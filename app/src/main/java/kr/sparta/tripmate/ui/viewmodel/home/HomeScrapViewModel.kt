fffa00ad92eaedb468ce9c071db11b15272c27e3
SeoJungHan <seojh5939@gmail.com>
2023-10-20 12:34:03 +0900

    [Refector] : 이름변경

---
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/HomeScrapViewModel.kt
@@ -0,0 +1,20 @@
+package kr.sparta.tripmate.ui.viewmodel.home
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kr.sparta.tripmate.data.repository.home.HomeFirstRepositoryImpl
+import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
+import kr.sparta.tripmate.domain.usecase.GetFirebaseScrapData
+import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
+
+class HomeScrapViewModel(private val getFirebaseScrapData: GetFirebaseScrapData) : ViewModel() {
+    private val _homeScraps : MutableLiveData<List<ScrapEntity>> = MutableLiveData()
+    val homeScraps get() = _homeScraps
+
+    fun updateScrapData(context: Context) {
+        val uid = SharedPreferences.getUid(context)
+        getFirebaseScrapData.invoke(uid, _homeScraps)
+    }
+}
