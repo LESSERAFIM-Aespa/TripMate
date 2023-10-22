2b8d7a44c5fe74e508dd3136f8f503b4d7259225
seongssu <loveforggong@gmail.com>
2023-10-23 01:23:58 +0900

    [refector] community : 북마크 관련 데이터클래스 변경

--- app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/main/CommunityModel.kt
@@ -11,11 +11,12 @@ data class CommunityModel(
     val body: String?, // 게시글 내용
     val profileNickname: String?, // 프로필 닉네임
     val profileThumbnail: String?, // 프로필 이미지
-    val views: String?, // 조회수
+    var views: String?, // 조회수
     var likes: String?, // 좋아요 수
     val key : String?,
-    val commuIsLike:Boolean = false
+    var commuIsLike:Boolean = false,
+    val boardLike:Boolean = false
 ) : Parcelable{
-        // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
-        constructor() : this("", null, null, null, null, null, null, null, null)
+    // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
+    constructor() : this("", null, null, null, null, null, null, null, null)
 }
