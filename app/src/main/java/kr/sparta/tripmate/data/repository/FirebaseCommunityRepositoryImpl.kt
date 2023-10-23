0d82548b2345bdfb2b52ce91790b5e60acc7b10d
seongssu <loveforggong@gmail.com>
2023-10-23 18:40:42 +0900

    [Feat] 북마크 키 관련 repositoryImpl수정

--- app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseCommunityRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseCommunityRepositoryImpl.kt
@@ -1,6 +1,7 @@
 import androidx.lifecycle.MutableLiveData
 import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
 import kr.sparta.tripmate.data.model.community.CommunityModel
+import kr.sparta.tripmate.domain.model.firebase.BoardKeyModelEntity
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
 import kr.sparta.tripmate.domain.repository.FirebaseCommunityRepository
@@ -12,9 +13,10 @@ class FirebaseCommunityRepositoryImpl(
     override fun getCommunityData(
         uid: String,
         commuLiveData: MutableLiveData<List<CommunityModelEntity?>>,
-        keyLiveData: MutableLiveData<List<KeyModelEntity?>>
+        keyLiveData: MutableLiveData<List<KeyModelEntity?>>,
+        boardKeyLiveData: MutableLiveData<List<BoardKeyModelEntity?>>
     ) {
-        remoteSource.getCommunityData(uid, commuLiveData, keyLiveData)
+        remoteSource.getCommunityData(uid, commuLiveData, keyLiveData, boardKeyLiveData)
     }
 
     override fun updateCommuIsLike(
