e56e387e599359d6a0d1ce278e93718f12b63d89
seongssu <loveforggong@gmail.com>
2023-10-23 18:38:09 +0900

    [Feat] 북마크 키 관련 도메인 레파지토리 수정했습니다.

--- app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseCommunityRepository.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseCommunityRepository.kt
@@ -3,13 +3,15 @@ package kr.sparta.tripmate.domain.repository
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import kr.sparta.tripmate.data.model.community.CommunityModel
+import kr.sparta.tripmate.domain.model.firebase.BoardKeyModelEntity
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
 
 interface FirebaseCommunityRepository {
     fun getCommunityData(
         uid: String, commuLiveData: MutableLiveData<List<CommunityModelEntity?>>,
-        keyLiveData: MutableLiveData<List<KeyModelEntity?>>
+        keyLiveData: MutableLiveData<List<KeyModelEntity?>>,  boardKeyLiveData :
+        MutableLiveData<List<BoardKeyModelEntity?>>
     )
     fun updateCommuIsLike(
         model: CommunityModel, position: Int, commuLiveData:
