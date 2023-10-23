5e95b85292ad2792bb93b9afc2b4f64e68536cfc
seongssu <loveforggong@gmail.com>
2023-10-23 18:31:05 +0900

    [Feat] 북마크 키 관련 usecase 추가했습니다.

--- app/src/main/java/kr/sparta/tripmate/domain/usecase/GetFirebaseCommunityData.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/GetFirebaseCommunityData.kt
@@ -1,6 +1,7 @@
 package kr.sparta.tripmate.domain.usecase
 
 import androidx.lifecycle.MutableLiveData
+import kr.sparta.tripmate.domain.model.firebase.BoardKeyModelEntity
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
 import kr.sparta.tripmate.domain.repository.FirebaseCommunityRepository
@@ -8,6 +9,7 @@ import kr.sparta.tripmate.domain.repository.FirebaseCommunityRepository
 class GetFirebaseCommunityData(private val repository: FirebaseCommunityRepository) {
     operator fun invoke(
         uid: String, commuLiveData: MutableLiveData<List<CommunityModelEntity?>>, keyLiveData
-        : MutableLiveData<List<KeyModelEntity?>>
+        : MutableLiveData<List<KeyModelEntity?>>, boardKeyLiveData :
+        MutableLiveData<List<BoardKeyModelEntity?>>
     ) = repository.getCommunityData(uid, commuLiveData, keyLiveData)
 }
