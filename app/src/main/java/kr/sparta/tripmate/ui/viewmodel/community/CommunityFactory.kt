ca28dcd1d5bb92eea74275bbc91ac33ff97fb154
seongssu <loveforggong@gmail.com>
2023-10-23 17:31:12 +0900

    [Feat] 커뮤니티 뷰모델 팩토리 추가

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/CommunityFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/CommunityFactory.kt
@@ -1,4 +1,27 @@
 package kr.sparta.tripmate.ui.viewmodel.community
 
-class CommunityFactory {
+import FirebaseCommunityRepositoryImpl
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
+import kr.sparta.tripmate.domain.repository.FirebaseCommunityRepository
+import kr.sparta.tripmate.domain.usecase.GetFirebaseCommunityData
+import kr.sparta.tripmate.domain.usecase.IsLikeFirebaseCommunityData
+import kr.sparta.tripmate.domain.usecase.IsViewsFirebaseCommunityData
+
+class CommunityFactory : ViewModelProvider.Factory {
+    private val repository: FirebaseCommunityRepository by lazy {
+        FirebaseCommunityRepositoryImpl(FirebaseDBRemoteDataSource())
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CommunityViewModel::class.java)) {
+            return CommunityViewModel(
+                GetFirebaseCommunityData(repository),
+                IsLikeFirebaseCommunityData(repository),
+                IsViewsFirebaseCommunityData(repository)
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
