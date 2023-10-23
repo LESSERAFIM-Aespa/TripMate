963a327655b7519a02e8aee801d402ebb4d43f83
SeoJungHan <seojh5939@gmail.com>
2023-10-23 20:44:25 +0900

    [Feat] : 원금, 잔액 데이터 화면표시 & 과정항목 데이터Load 메서드 구현

--- app/src/main/java/kr/sparta/tripmate/ui/budget/detail/procedure/BudgetDetailProcedureFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/detail/procedure/BudgetDetailProcedureFragment.kt
@@ -1,12 +1,18 @@
 package kr.sparta.tripmate.ui.budget.detail.procedure
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import kr.sparta.tripmate.databinding.FragmentBudgetDetailProcedureBinding
+import kr.sparta.tripmate.ui.budget.detail.main.BudgetDetailActivity
+import kr.sparta.tripmate.ui.viewmodel.budget.procedure.BudgetProcedureFactory
+import kr.sparta.tripmate.ui.viewmodel.budget.procedure.BudgetProcedureViewModel
 
 
 /**
@@ -18,12 +24,25 @@ class BudgetDetailProcedureFragment : Fragment() {
         fun newInstance() = BudgetDetailProcedureFragment()
     }
 
+    private lateinit var procedureContext: Context
+    private lateinit var activity: BudgetDetailActivity
+
     private var _binding: FragmentBudgetDetailProcedureBinding? = null
     private val binding: FragmentBudgetDetailProcedureBinding
         get() = _binding!!
 
     private val adapter by lazy {
         BudgetDetailProcedureListAdapter()
+    }
+
+    private val viewModel: BudgetProcedureViewModel by viewModels() {
+        BudgetProcedureFactory()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        procedureContext = context
+        activity = context as BudgetDetailActivity
     }
 
     override fun onCreateView(
@@ -39,10 +58,63 @@ class BudgetDetailProcedureFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initViews()
+        initViewModel()
     }
 
     private fun initViews() = with(binding) {
+        /**
+         * 작성자: 서정한
+         * 내용: 과정 데이터 init
+         * */
+        fun initProcedureData() {
+            /**
+             * 작성자: 서정한
+             * 내용: 금액에 콤마 추가
+             * */
+            fun setCommaForMoneeyText(money: String): String {
+                val commaCount = if (money.length % 3 == 0) {
+                    (money.length / 3) - 1
+                } else {
+                    money.length / 3
+                }
+                var commaStartIndex = money.length % 3
+                val comma = ','
+                val stringBuilder = StringBuilder()
+                stringBuilder.append(money)
+
+                for (i in 0 until commaCount) {
+                    stringBuilder.insert(commaStartIndex, comma)
+                    commaStartIndex += 4
+                }
+
+                return "${stringBuilder}원"
+            }
+
+            activity.budget?.let {
+                // 원금
+                budgetDetailStatusPrincipalTextview.text = setCommaForMoneeyText(it.money.toString())
+                // 잔액
+                budgetDetailStatusBalanceTextview.text = setCommaForMoneeyText(it.money.toString())
+                // 과정 RecyclerView item
+                viewModel.getAllProcedures(it)
+            }
+        }
+
+        // RecyclerView
         budgetDetailProcedureRecyclerview.adapter = adapter
+
+        initProcedureData()
+    }
+
+    private fun initViewModel() {
+        viewModel.procedureList.observe(viewLifecycleOwner) { list ->
+            adapter.submitList(list)
+
+            // 잔액 업데이트
+            if (list.isNotEmpty()) {
+                binding.budgetDetailStatusBalanceTextview.text = list.last().totalAmount.toString()
+            }
+        }
     }
 
     override fun onDestroy() {
