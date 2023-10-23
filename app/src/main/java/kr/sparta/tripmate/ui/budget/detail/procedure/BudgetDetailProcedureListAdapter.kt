f0f0325df920696966482ad7d3100180d73c175b
SeoJungHan <seojh5939@gmail.com>
2023-10-23 20:44:57 +0900

    [Refector] : 코드정리

--- app/src/main/java/kr/sparta/tripmate/ui/budget/detail/procedure/BudgetDetailProcedureListAdapter.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/detail/procedure/BudgetDetailProcedureListAdapter.kt
@@ -49,8 +49,8 @@ class BudgetDetailProcedureListAdapter :
             budgetProcedureBeforeMoneyTextview.setText(item.beforeMoney)
             budgetProcedurePriceTextview.setText(item.price)
             budgetProcedureTotalAmountTextview.setText(item.totalAmount)
-            budgetProcedureTitleTextview.setText(item.title)
-            budgetProcedureTimeTextview.setText(item.time)
+            budgetProcedureTitleTextview.text = item.title
+            budgetProcedureTimeTextview.text = item.time
             budgetProcedureCategoryTextview.setText(item.categoryName)
             // TODO 카테고리 컬러값 어떻게 구현할지 방법찾아보기!
             // TODO DB에서 가져온 Procedure item들 ProcedureModel에 넣어서 사용하기
