0524e216a2cba1d027928681e550972cd6820d50
seongssu <loveforggong@gmail.com>
2023-10-23 17:32:49 +0900

    [Feat] 커뮤니티 뷰모델 추가

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/CommunityViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/CommunityViewModel.kt
@@ -14,166 +14,52 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kr.sparta.tripmate.data.model.community.CommunityModel
 import kr.sparta.tripmate.data.model.community.KeyModel
+import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
+import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
+import kr.sparta.tripmate.domain.model.firebase.toCommunity
+import kr.sparta.tripmate.domain.usecase.GetFirebaseCommunityData
+import kr.sparta.tripmate.domain.usecase.IsLikeFirebaseCommunityData
+import kr.sparta.tripmate.domain.usecase.IsViewsFirebaseCommunityData
 import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
-class CommunityViewModel : ViewModel() {
-    private val _dataModelList = MutableLiveData<MutableList<CommunityModel>>()
-    val dataModelList: LiveData<MutableList<CommunityModel>> get() = _dataModelList
+class CommunityViewModel(
+    private val getFirebaseCommunityData: GetFirebaseCommunityData,
+    private val isLikeFirebaseCommunityData: IsLikeFirebaseCommunityData,
+    private val isViewsFirebaseCommunityData: IsViewsFirebaseCommunityData
+) :
+    ViewModel() {
+
+    private val _dataModelList: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
+    val dataModelList get() = _dataModelList
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
-    private val _keyList = MutableLiveData<MutableList<KeyModel>>()
-    val keyList: LiveData<MutableList<KeyModel>> get() = _keyList
-    private val database = Firebase.database
+    private val _keyModelList: MutableLiveData<List<KeyModelEntity?>> = MutableLiveData()
     fun updateDataModelList(context: Context) = viewModelScope.launch {
         kotlin.runCatching {
+            val uid = SharedPreferences.getUid(context)
             _isLoading.value = true
-            loadCommunity(context)
+            getFirebaseCommunityData.invoke(uid, _dataModelList, _keyModelList)
             _isLoading.value = false
         }
     }
 
-    private fun loadCommunity(context: Context) {
-        val comuRef = database.getReference("CommunityData")
-        comuRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val allCommunityData = arrayListOf<CommunityModel>()
-
-                for (userPostSnapshot in snapshot.children) {
-                    val postModel = userPostSnapshot.getValue(CommunityModel::class.java)
-                    if (postModel != null) {
-                        allCommunityData.add(postModel)
-                    }
-                }
-                if (!allCommunityData.isNullOrEmpty()) {
-                    myLoadCommunity(allCommunityData, context)
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
-    }
-    private fun myLoadCommunity(allCommunityData: ArrayList<CommunityModel>, context: Context) {
-        val mycommuRef = database.getReference("MyKey").child(SharedPreferences.getUid(context))
-        mycommuRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val myKeyData = arrayListOf<KeyModel>()
-
-                for (item in snapshot.children) {
-                    val getMyKey = item.getValue(KeyModel::class.java)
-                    if (getMyKey != null) {
-                        myKeyData.add(getMyKey)
-                    }
-                }
-                if (!myKeyData.isNullOrEmpty()) {
-                    for (item in allCommunityData) {
-                        for (myItem in myKeyData) {
-                            if (item.id == myItem.uid && item.key == myItem.key) {
-                                item.commuIsLike = myItem.myCommuIsLike
-                            }
-                        }
-                    }
-                }
-                _keyList.value = myKeyData
-                _dataModelList.value = allCommunityData
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
-
-    fun updateCommuIsLike(model: CommunityModel, position: Int) {
-        //or.Empty() : 해당값이 null일때 빈 리스트를 반환해준다.
-        val list = dataModelList.value.orEmpty().toMutableList()
-        list.find { it.key == model.key } ?: return
-
-        list[position] = model
-
-        val currentLikes = list[position].likes?.toIntOrNull() ?: 0
-        val newLikes = if (list[position].commuIsLike) {
-            currentLikes + 1
-        } else {
-            if (currentLikes >= 1) {
-                currentLikes - 1
-            } else {
-                currentLikes
+    fun updateCommuIsLike(model: CommunityModelEntity, position: Int, context: Context) =
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val uid = SharedPreferences.getUid(context)
+                isLikeFirebaseCommunityData.invoke(
+                    model.toCommunity(),
+                    position,
+                    _dataModelList,
+                    _keyModelList,
+                    uid
+                )
             }
         }
-        list[position].likes = newLikes.toString()
-        _dataModelList.value = list
 
-
-        val commuDatabase = Firebase.database
-
-        val updateList = arrayListOf<CommunityModel>()
-        list?.let {
-            updateList.addAll(it)
+    fun updateCommuView(model: CommunityModelEntity, position: Int) = viewModelScope.launch {
+        kotlin.runCatching {
+            isViewsFirebaseCommunityData.invoke(model.toCommunity(), position, _dataModelList)
         }
-        val commuRef = commuDatabase.getReference("CommunityData")
-        commuRef.setValue(updateList)
-    }
-    fun updateCommuIsLike(model: CommunityModel, position: Int, context: Context) {
-        //or.Empty() : 해당값이 null일때 빈 리스트를 반환해준다.
-        val list = dataModelList.value.orEmpty().toMutableList()
-        list.find { it.key == model.key } ?: return
-
-        list[position] = model
-
-        val currentLikes = list[position].likes?.toIntOrNull() ?: 0
-        val newLikes = if (list[position].commuIsLike) {
-            currentLikes + 1
-        } else {
-            if (currentLikes >= 1) {
-                currentLikes - 1
-            } else {
-                currentLikes
-            }
-        }
-        val mycommuRef = database.getReference("MyKey").child(SharedPreferences.getUid(context))
-        val myKeyModel =
-            KeyModel(list[position].id, list[position].key, list[position].commuIsLike)
-        val myKeyList = keyList.value.orEmpty().toMutableList()
-        val selectedKey = myKeyList.find { it.key == myKeyModel.key }
-        if (selectedKey != null) {
-            selectedKey.myCommuIsLike = myKeyModel.myCommuIsLike
-        } else {
-            myKeyList.add(myKeyModel)
-        }
-        mycommuRef.setValue(myKeyList)
-        _keyList.value = myKeyList
-
-        list[position].likes = newLikes.toString()
-
-        val commuRef = database.getReference("CommunityData")
-        val updateList = arrayListOf<CommunityModel>()
-        list?.let {
-            updateList.addAll(it)
-            updateList.forEach { it.commuIsLike = false }
-        }
-        commuRef.setValue(updateList)
-        _dataModelList.value = list
-    }
-    fun updateCommuView(model: CommunityModel, position: Int) {
-        val items = dataModelList.value.orEmpty().toMutableList()
-        val selectedItem = items.find { it.key == model.key } ?: return
-        items[position] = model
-        val currentViews = selectedItem.views?.toIntOrNull() ?: 0
-        val newViews = currentViews + 1
-
-        items[position].views = newViews.toString()
-        Log.d("TripMates", "뷰 : ${items[position].views}")
-
-        val commuRef = database.getReference("CommunityData")
-        val updateList = arrayListOf<CommunityModel>()
-        items?.let {
-            updateList.addAll(it)
-        }
-        commuRef.setValue(updateList)
-        _dataModelList.value = items
-
     }
 }
