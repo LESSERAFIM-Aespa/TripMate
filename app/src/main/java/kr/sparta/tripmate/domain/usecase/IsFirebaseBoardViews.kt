3d4ddee2ee163a8a9caef0a73550ad2f910d9792
seongssu <loveforggong@gmail.com>
2023-10-25 05:41:07 +0900

    [Feat] Home : 홈에서도 게시글 클릭 시 Views가 1 오르게 하기 위해서 도메인 usecase 추가

---
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/IsFirebaseBoardViews.kt
@@ -0,0 +1,12 @@
+package kr.sparta.tripmate.domain.usecase
+
+import androidx.lifecycle.MutableLiveData
+import kr.sparta.tripmate.data.model.community.CommunityModel
+import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
+import kr.sparta.tripmate.domain.repository.FirebaseBoardRepository
+
+class IsFirebaseBoardViews(private val repository:FirebaseBoardRepository) {
+    operator fun invoke(model: CommunityModel, position: Int, commuLiveData:
+    MutableLiveData<List<CommunityModelEntity?>>
+    ) = repository.isCommuView(model, position, commuLiveData)
+}
