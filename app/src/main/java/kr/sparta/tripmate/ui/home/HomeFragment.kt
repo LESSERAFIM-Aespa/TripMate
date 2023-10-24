225abdb6aca5bf5e75ea80494c99ccbcd3fed069
seongssu <loveforggong@gmail.com>
2023-10-24 12:29:30 +0900

    [Feat] MyPage에서 북마크된 Scrap데이터 추가&삭제 시 갱신됩니다.

--- app/src/main/java/kr/sparta/tripmate/ui/home/HomeFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/home/HomeFragment.kt
@@ -104,7 +104,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         observeViewModel()
     }
 
