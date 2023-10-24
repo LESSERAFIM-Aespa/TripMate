b8d65f245c120cf2385d0770a83423050b598f5c
seongssu <loveforggong@gmail.com>
2023-10-25 05:42:03 +0900

    [Feat] Home : 홈에서도 게시글 클릭 시 Views가 1 오르게 하기 위해서 뷰모델& 라이브데이터 적용

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
@@ -3,13 +3,22 @@ package kr.sparta.tripmate.ui.viewmodel.home.board
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
+import kr.sparta.tripmate.domain.model.firebase.toCommunity
 import kr.sparta.tripmate.domain.usecase.GetFirebaseBoardData
+import kr.sparta.tripmate.domain.usecase.IsFirebaseBoardViews
 
-class HomeBoardViewModel(private val getFirebaseBoardData: GetFirebaseBoardData) : ViewModel() {
+class HomeBoardViewModel(private val getFirebaseBoardData: GetFirebaseBoardData, private val
+isFirebaseBoardViews: IsFirebaseBoardViews
+) :
+    ViewModel
+    () {
     private val _homeBoard: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
     val homeBoard get() = _homeBoard
 
     fun getHomeBoardData(uid: String) {
         getFirebaseBoardData.invoke(uid, _homeBoard)
+    }
+    fun viewHomeBoardData(model:CommunityModelEntity,position:Int){
+        isFirebaseBoardViews.invoke(model.toCommunity(), position,_homeBoard)
     }
 }
