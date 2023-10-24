9e394d6e19328a40ea3ab6d07687f3d96feecf1b
seongssu <loveforggong@gmail.com>
2023-10-24 19:25:10 +0900

    [Feat] Home/ Board 데이터 관리를 위해 뷰모델& 팩토리를 추가했습니다.

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
@@ -1,4 +1,15 @@
 package kr.sparta.tripmate.ui.viewmodel.home.board
 
-class HomeBoardViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
+import kr.sparta.tripmate.domain.usecase.GetFirebaseBoardData
+
+class HomeBoardViewModel(private val getFirebaseBoardData: GetFirebaseBoardData) : ViewModel() {
+    private val _homeBoard: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
+    val homeBoard get() = _homeBoard
+
+    fun getHomeBoardData(uid: String) {
+        getFirebaseBoardData.invoke(uid, _homeBoard)
+    }
 }
