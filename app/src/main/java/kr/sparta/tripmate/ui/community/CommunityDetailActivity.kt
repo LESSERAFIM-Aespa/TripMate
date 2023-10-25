9bc4b0e2b2d47c8f862763a29c6c13f2b55b50b6
SeoJungHan <seojh5939@gmail.com>
2023-10-25 21:59:10 +0900

    [Refector] : 커뮤니티 상세페이지 intent 등 코드 리펙토링

--- app/src/main/java/kr/sparta/tripmate/ui/community/CommunityDetailActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/CommunityDetailActivity.kt
@@ -1,44 +1,73 @@
 package kr.sparta.tripmate.ui.community
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import coil.load
 import kr.sparta.tripmate.databinding.ActivityCommunityDetailBinding
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 
 class CommunityDetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_ENTITY = "extra_entity"
+        fun newIntentForEntity(context: Context, model: CommunityModelEntity): Intent =
+            Intent(context, CommunityDetailActivity::class.java).apply {
+                putExtra(EXTRA_ENTITY, model)
+            }
+    }
 
-    private lateinit var binding: ActivityCommunityDetailBinding
-    private lateinit var writeItem: CommunityModelEntity
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityCommunityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        //데이터를 받는 부분
-        writeItem = intent.getParcelableExtra<CommunityModelEntity>("Data")!!
+    private val binding: ActivityCommunityDetailBinding by lazy {
+        ActivityCommunityDetailBinding.inflate(layoutInflater)
+    }
 
-        Log.d("CommunityDetailfxxk",writeItem.addedImage.toString())
-        if (!writeItem.addedImage.isNullOrBlank()) {
-            binding.communityIvAddImage.visibility = View.VISIBLE
-            setupViews()
+    private val model by lazy {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(EXTRA_ENTITY, CommunityModelEntity::class.java)
         } else {
-            binding.communityIvAddImage.visibility = View.GONE
-            setupViews()
-        }
-
-        binding.communityDetailBackbutton.setOnClickListener {
-            finish()
+            intent.getParcelableExtra(EXTRA_ENTITY)
         }
     }
-    private fun setupViews() {
-        binding.communityUserprofile.load(writeItem.profileThumbnail)
-        binding.communityTvDetailTitle.text = writeItem.title
-        binding.communityTvDetailDescription.text = writeItem.body
-        binding.communityTvDetailUsername.text = writeItem.profileNickname
-        binding.communityIvAddImage.load(writeItem.addedImage)
-        binding.communityDetailLikecount.text = writeItem.likes
-        binding.communityTvDetailViewcount.text = writeItem.views
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+        /**
+         * 작성자: 서정한
+         * 내용: 이미지Url유무에따라 ImageView의 Visible을 조절합니다.
+         * */
+        fun toggleImgUrlIsEmpty(model: CommunityModelEntity) {
+            model?.addedImage?.let { url ->
+                if (url.isNotEmpty()) {
+                    communityImageCardView.visibility = View.VISIBLE
+                } else {
+                    communityImageCardView.visibility = View.GONE
+                }
+            }
+        }
+
+        // View init
+        model?.let {
+            toggleImgUrlIsEmpty(it)
+            communityUserprofile.load(it.profileThumbnail)
+            communityTvDetailTitle.text = it.title
+            communityTvDetailDescription.text = it.description
+            communityTvDetailUsername.text = it.profileNickname
+            communityImageIv.load(it.addedImage)
+            communityDetailLikecount.text = it.likes
+            communityTvDetailViewcount.text = it.views
+        }
+
+        // 뒤로가기
+        communityDetailToolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
