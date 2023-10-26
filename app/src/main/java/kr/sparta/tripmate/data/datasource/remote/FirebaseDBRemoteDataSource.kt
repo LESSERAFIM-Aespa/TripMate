4a613fca950693eb9beacba441269c543d330ada
seongssu <loveforggong@gmail.com>
2023-10-27 05:44:58 +0900

    [Feat] setting : remoteDataSource 회원탈퇴 기능을 추가 했습니다.

--- app/src/main/java/kr/sparta/tripmate/data/datasource/remote/FirebaseDBRemoteDataSource.kt
+++ app/src/main/java/kr/sparta/tripmate/data/datasource/remote/FirebaseDBRemoteDataSource.kt
@@ -14,7 +14,7 @@ import com.google.firebase.ktx.Firebase
 import kr.sparta.tripmate.data.model.community.BoardKeyModel
 import kr.sparta.tripmate.data.model.community.CommunityModel
 import kr.sparta.tripmate.data.model.community.KeyModel
-import kr.sparta.tripmate.data.model.community.UserData
+import kr.sparta.tripmate.data.model.login.UserData
 import kr.sparta.tripmate.data.model.scrap.ScrapModel
 import kr.sparta.tripmate.domain.model.firebase.BoardKeyModelEntity
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
@@ -25,6 +25,7 @@ import kr.sparta.tripmate.domain.model.firebase.toEntity
 import kr.sparta.tripmate.domain.model.login.UserDataEntity
 import kr.sparta.tripmate.domain.model.login.toEntity
 import kr.sparta.tripmate.util.method.shortToast
+import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
 /**
  * 작성자: 서정한
@@ -453,10 +454,20 @@ class FirebaseDBRemoteDataSource {
      * 내용 : 자기소개를 수정하고 저장 후 업데이트 합니다.
      * 추후 프로필 사진을 수정하거나 다른 데이터를 수정해도 재사용 가능합니다.
      */
-    fun saveUserData(model: UserDataEntity, userLiveData: MutableLiveData<UserDataEntity?>) {
+    fun saveUserData(
+        model: UserDataEntity, context: Context, userLiveData:
+        MutableLiveData<UserDataEntity?>
+    ) {
         val userRef = fireDatabase.getReference("UserData").child(model.login_Uid!!)
         userLiveData.postValue(model)
         userRef.setValue(model)
+    }
+
+    fun resignUserData(context: Context) {
+        val userRef = fireDatabase.getReference("UserData").child(SharedPreferences.getUid(context))
+        userRef.removeValue().addOnSuccessListener {
+            context.shortToast("정상적으로 탈퇴 되었습니다.")
+        }
     }
 
     private fun bookMarkToast(context: Context, selectedBoardKey: Boolean) {
