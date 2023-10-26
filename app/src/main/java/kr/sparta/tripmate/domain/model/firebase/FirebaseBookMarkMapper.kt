70425ba51a06f87734604940540a815ee443a99e
SeoJungHan <seojh5939@gmail.com>
2023-10-26 22:26:45 +0900

    [Refector] : 코드정리

--- app/src/main/java/kr/sparta/tripmate/domain/model/firebase/FirebaseBookMarkMapper.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/model/firebase/FirebaseBookMarkMapper.kt
@@ -3,7 +3,7 @@ package kr.sparta.tripmate.domain.model.firebase
 fun ScrapEntity.toCommunityEntity() = CommunityModelEntity(
     id = "",
     title = title,
-    body = description,
+    description = description,
     profileNickname = bloggername,
     profileThumbnail = null,
     views = "",
