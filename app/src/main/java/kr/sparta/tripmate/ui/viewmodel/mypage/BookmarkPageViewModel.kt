c4fff915326d3431919683849c28d7d98a083a51
seongssu <loveforggong@gmail.com>
2023-10-26 10:40:52 +0900

    [Feat] mypage : 커뮤니티& 블로그 데이터를 공용으로 쓰기위해 뷰모델 수정

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/mypage/BookmarkPageViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/mypage/BookmarkPageViewModel.kt
@@ -3,20 +3,54 @@ package kr.sparta.tripmate.ui.viewmodel.mypage
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
-import kr.sparta.tripmate.domain.usecase.GetFirebaseScrapData
+import kr.sparta.tripmate.domain.model.firebase.toCommunity
+import kr.sparta.tripmate.domain.usecase.firebasescraprepository.GetFirebaseBoardDataFromScrapRepo
+import kr.sparta.tripmate.domain.usecase.firebasescraprepository.GetFirebaseScrapData
+import kr.sparta.tripmate.domain.usecase.firebasescraprepository.UpdateCommuIsViewFromScrapRepo
+import kr.sparta.tripmate.util.ScrapInterface
 import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
-class BookmarkPageViewModel(private val getFirebaseScrapData: GetFirebaseScrapData): ViewModel() {
-    // 내정보 스크랩 LiveData
-    private val _mypageScraps : MutableLiveData<List<ScrapEntity?>> = MutableLiveData()
-    val myPageList get() = _mypageScraps
-//    private val _mypagePosts : MutableLiveData<List<BoardModel>> = MutableLiveData()
-//    val mypagePosts get() = _mypagePosts
+class BookmarkPageViewModel(
+    private val getFirebaseScrapData: GetFirebaseScrapData,
+    private val getFirebaseBoardDataFromScrapRepo: GetFirebaseBoardDataFromScrapRepo,
+    private val updateCommuIsViewFromScrapRepo: UpdateCommuIsViewFromScrapRepo
+) : ViewModel() {
 
-    // Firebase RDB에서 Scrap데이터를 가져온 결과를 livedata에 업데이트 해준다.
-    fun updateScrapData(context: Context) {
-        val uid = SharedPreferences.getUid(context)
-        getFirebaseScrapData.invoke(uid, _mypageScraps)
+    private val _mypageScraps: MutableLiveData<List<ScrapEntity?>> = MutableLiveData()
+    val myPageList get() = _mypageScraps
+    private val _mypageBoard: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
+    val mypageBoard get() = _mypageBoard
+
+    val _totalMyPage: MutableLiveData<List<ScrapInterface?>> = MutableLiveData()
+    val totalMyPage get() = _totalMyPage
+
+    fun updateScrapData(context: Context) = viewModelScope.launch {
+        kotlin.runCatching {
+            val uid = SharedPreferences.getUid(context)
+            getFirebaseScrapData.invoke(uid, _mypageScraps)
+        }
+
+    }
+
+    fun updateBoardData(uid: String) = viewModelScope.launch {
+        kotlin.runCatching {
+            getFirebaseBoardDataFromScrapRepo.invoke(uid, _mypageBoard)
+        }
+    }
+
+    fun updateBoardDataView(model: CommunityModelEntity, position: Int) {
+        updateCommuIsViewFromScrapRepo.invoke(model.toCommunity(), position, _mypageBoard)
+    }
+
+    fun mergeScrapAndBoardData() {
+            val scraps = _mypageScraps.value ?: emptyList()
+            val boardData = _mypageBoard.value ?: emptyList()
+
+            _totalMyPage.value = scraps + boardData
+
     }
 }
