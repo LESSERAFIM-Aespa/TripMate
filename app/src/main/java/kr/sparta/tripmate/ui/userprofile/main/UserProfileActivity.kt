69c03dcf9572c2c69b6db1ec0e3375205a2af45e
seongssu <loveforggong@gmail.com>
2023-10-28 07:12:11 +0900

    [Feat] userprofile : RDB에 저장된 유저정보를 화면에 표시해줍니다.

--- app/src/main/java/kr/sparta/tripmate/ui/userprofile/main/UserProfileActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/userprofile/main/UserProfileActivity.kt
@@ -5,10 +5,17 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import coil.load
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
 import kr.sparta.tripmate.databinding.ActivityUserProfileBinding
+import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.ui.userprofile.model.UserProfileModel
+import kr.sparta.tripmate.ui.viewmodel.userproflie.UserProfileFactory
+import kr.sparta.tripmate.ui.viewmodel.userproflie.UserProfileViewModel
+import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
 /**
  * 작성자: 서정한
@@ -17,23 +24,25 @@ import kr.sparta.tripmate.ui.userprofile.model.UserProfileModel
 class UserProfileActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_MODEL = "extra_model"
-        fun newIntentForGetUserProfile(context: Context, model: UserProfileModel): Intent =
+        fun newIntentForGetUserProfile(context: Context, model: CommunityModelEntity): Intent =
             Intent(context, UserProfileActivity::class.java).apply {
                 putExtra(EXTRA_MODEL, model)
             }
     }
 
+    private lateinit var auth: FirebaseAuth
     private val binding by lazy {
         ActivityUserProfileBinding.inflate(layoutInflater)
     }
 
-    private val adapter by lazy{
+    private val adapter by lazy {
         UserProfileTabLayoutAdapter(this@UserProfileActivity)
     }
+    private val userProfileViewModel: UserProfileViewModel by viewModels { UserProfileFactory() }
 
     private val model by lazy {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra(EXTRA_MODEL, UserProfileModel::class.java)
+            intent.getParcelableExtra(EXTRA_MODEL, CommunityModelEntity::class.java)
         } else {
             intent.getParcelableExtra(EXTRA_MODEL)
         }
@@ -42,29 +51,40 @@ class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        SharedPreferences.saveUidFromUser(this, model!!.id)
         initView()
+        initViewModel()
+        callDataSource()
+    }
+
+    private fun callDataSource() {
+        model?.let {
+            userProfileViewModel.updateUserData(model!!.id)
+        }
+    }
+
+    private fun initViewModel() {
+        with(userProfileViewModel) {
+            userProfileResult.observe(this@UserProfileActivity) {
+                with(binding) {
+                    userProfileThumbnail.load(it?.login_profile)
+                    userProfileNickname.text = it?.login_NickName
+                    userProfileContentTextview.text = it?.login_coment
+                }
+            }
+        }
     }
 
     private fun initView() = with(binding) {
         userProfileViewpager.adapter = adapter
         // TabLayout x ViewPager2
-        TabLayoutMediator(userProfileTablayout, userProfileViewpager) {tab, position ->
+        TabLayoutMediator(userProfileTablayout, userProfileViewpager) { tab, position ->
             tab.setText(adapter.getTitle(position))
         }.attach()
 
         // 뒤로가기
         userProfileToolbar.setNavigationOnClickListener {
             finish()
-        }
-
-        model?.let {
-            // 썸네일
-            userProfileThumbnail.load(it.thumbnail)
-            // 닉네임
-            userProfileNickname.text = it.nickname
-            // 자기소개 한줄
-            userProfileContentTextview.text = it.selfContent
         }
     }
 }
