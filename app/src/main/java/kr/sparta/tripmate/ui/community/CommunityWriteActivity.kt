fc8d4dc8160c74179e8bcd49ced4bacf8e5bad67
SeoJungHan <seojh5939@gmail.com>
2023-10-27 18:07:27 +0900

    [Feat] : 기존글 수정시 DetailPage로 업데이트를 위한 모델전달기능 구현

--- app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/community/CommunityWriteActivity.kt
@@ -2,6 +2,7 @@ package kr.sparta.tripmate.ui.community
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +10,19 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import coil.load
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import kr.sparta.tripmate.databinding.ActivityCommunityWriteBinding
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
 import kr.sparta.tripmate.ui.viewmodel.community.write.CommunityWriteFactory
 import kr.sparta.tripmate.ui.viewmodel.community.write.CommunityWriteViewModel
+import kr.sparta.tripmate.util.method.isWindowTouchable
 import kr.sparta.tripmate.util.method.shortToast
 import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 import java.util.regex.Pattern
@@ -61,6 +66,7 @@ class CommunityWriteActivity : AppCompatActivity() {
     private val imageLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
+                binding.communityWriteImageIcon.visibility = View.GONE
                 val imageUri = result.data?.data
                 imageUri?.let { binding.communityWriteImage.setImageURI(it) }
             }
@@ -79,7 +85,12 @@ class CommunityWriteActivity : AppCompatActivity() {
         model?.let {
             communityWriteTitle.setText(it.title)
             communityWriteDescription.setText(it.description)
-            communityWriteImage.load(it.addedImage)
+            if(it.addedImage == "") {
+                communityWriteImageIcon.visibility = View.VISIBLE
+            } else {
+                communityWriteImageIcon.visibility = View.INVISIBLE
+                communityWriteImage.load(it.addedImage)
+            }
         }
 
         // 뒤로가기
@@ -121,8 +132,19 @@ class CommunityWriteActivity : AppCompatActivity() {
 
         // 게시
         communityWritePost.setOnClickListener {
+            /**
+             * 작성자: 서정한
+             * 내용: 새로운 글을 작성하여 Firebase RDB로 업로드합니다.
+             * RDB의 경우 기존자료를 업데이트할때에도 새로운 리스트를 만들어 덮어씌우는 형태입니다.
+             * 그래서 수정하는경우에도 새롭게작성하는것과 로직상 차이가 없습니다.
+             * */
             fun postWrite() {
-                val bitmap = (binding.communityWriteImage.drawable as BitmapDrawable).bitmap
+                val bitmap: Bitmap? = if(communityWriteImage.drawable != null) {
+                    (communityWriteImage.drawable as BitmapDrawable).bitmap
+                } else {
+                    null
+                }
+
                 val key = viewModel.getCommunityKey()
                 val imgName = key.substring(key.length - 17, key.length)
                 val model = CommunityModelEntity(
@@ -139,6 +161,7 @@ class CommunityWriteActivity : AppCompatActivity() {
                     boardIsLike = false,
                 )
 
+                // 새 글 작성 or Edit
                 viewModel.updateCommunityWrite(
                     imgName = imgName,
                     image = bitmap,
@@ -146,19 +169,22 @@ class CommunityWriteActivity : AppCompatActivity() {
                 )
             }
 
-            if (binding.communityWriteTitle.text.toString().trim() == "" && binding.communityWriteDescription.text.toString().trim() == "") {
+            if (binding.communityWriteTitle.text.toString()
+                    .trim() == "" && binding.communityWriteDescription.text.toString().trim() == ""
+            ) {
                 shortToast("제목과 내용을 입력해주셔야 합니다.")
                 return@setOnClickListener
             }
 
             // 로딩시작
             viewModel.setLoadingState(true)
+
             // 새로운 글 생성
             postWrite()
         }
 
         // 기기이미지 불러오기
-        communityWriteImage.setOnClickListener {
+        communityWriteCardView.setOnClickListener {
             val gallery =
                 Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             imageLauncher.launch(gallery)
@@ -166,15 +192,42 @@ class CommunityWriteActivity : AppCompatActivity() {
     }
 
     private fun initViewModel() {
-        viewModel.isLoading.observe(this) { isLoading ->
-            if (isLoading) {
-                binding.communityWriteProgressBar.visibility = View.VISIBLE
-            } else {
-                binding.communityWriteProgressBar.visibility = View.GONE
-                shortToast("글이 생성되었습니다.")
-                // 업로드가 완료될때 Activity종료
-                finish()
+        with(viewModel) {
+            // 로딩처리
+            isLoading.observe(this@CommunityWriteActivity) { isLoading ->
+                if (isLoading) {
+                    binding.communityWriteProgressBar.visibility = View.VISIBLE
+                    // 화면터치 막기
+                    isWindowTouchable(this@CommunityWriteActivity, true)
+                } else {
+                    binding.communityWriteProgressBar.visibility = View.GONE
+                    shortToast("글이 생성되었습니다.")
+                }
             }
+
+            // 발행된 아이템 받은 후 DetailPage에 전달
+            publishSubject.subscribeBy(
+                onNext = {
+                    // 로딩 중지
+                    setLoadingState(false)
+
+                    // 업로드 완료후 업데이트된 model DetailPage에 전달
+                    val intent =
+                        CommunityDetailActivity.newIntentForEntity(this@CommunityWriteActivity, it)
+                    setResult(RESULT_OK, intent)
+
+                    // 화면터치 해제
+                    isWindowTouchable(this@CommunityWriteActivity, false)
+
+                    finish()
+                },
+                onError = {
+                    it.printStackTrace()
+                },
+                onComplete = {
+                    Log.d("TripMates", "Complete WriteActivity")
+                }
+            )
         }
     }
 
