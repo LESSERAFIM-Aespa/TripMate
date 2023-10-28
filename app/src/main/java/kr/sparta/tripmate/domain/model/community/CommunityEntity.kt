797837b80db33a645ae4f2804da13593c61ea133
SeoJungHan <seojh5939@gmail.com>
2023-10-29 01:24:25 +0900

    [Refector] : 게시판기능 RemoteDataSource , Repository 정의

--- app/src/main/java/kr/sparta/tripmate/domain/model/community/CommunityEntity.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/model/community/CommunityEntity.kt
@@ -1,11 +1,12 @@
-package kr.sparta.tripmate.data.model.community
+package kr.sparta.tripmate.domain.model.community
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kr.sparta.tripmate.util.ScrapInterface
 
 @Parcelize
-data class CommunityModel(
-    val key : String?, // 각 게시판의 고유 id
+data class CommunityEntity(
+    val key: String?, // 각 게시판의 고유 id
     val title: String?, // 제목
     val description: String?, // 게시글 내용
     val userid: String?, // 글 작성자 id
@@ -14,8 +15,9 @@ data class CommunityModel(
     val views: String?, // 조회수
     val likes: String?, // 좋아요 수
     val image: String?, // 이미지
-    val isLike: Boolean =false, // 좋아요
-) : Parcelable{
+    val isLike: Boolean = false, // 좋아요
+
+) : Parcelable, ScrapInterface {
     // 매개 변수 없는 생성자 추가 : 파이어베이스는 불러올때 빈생성자를 받아서 이렇게 사용해야함
     constructor() : this(null, null, null, null, null, null, null, null, null, false)
 }
