0ffde6acb70e4d0c3e245ae5fd48738086a19138
seongssu <loveforggong@gmail.com>
2023-10-28 13:14:49 +0900

    [Feat] scrapdetail : 블로그 북마크 삭제 기능 추가

--- app/src/main/java/kr/sparta/tripmate/ui/scrap/ScrapDetail.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/scrap/ScrapDetail.kt
@@ -7,12 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebViewClient
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kr.sparta.tripmate.R
 import kr.sparta.tripmate.databinding.ActivityScrapDetailBinding
 import kr.sparta.tripmate.data.model.scrap.ScrapModel
 import kr.sparta.tripmate.domain.model.firebase.ScrapEntity
+import kr.sparta.tripmate.ui.viewmodel.scrap.ScrapDetailFactory
+import kr.sparta.tripmate.ui.viewmodel.scrap.ScrapDetailViewModel
+import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
 class ScrapDetail : AppCompatActivity() {
     companion object {
@@ -22,7 +27,7 @@ class ScrapDetail : AppCompatActivity() {
                 putExtra(EXTRA_MODEL, model)
             }
     }
-
+    private val scrapDetailViewModel: ScrapDetailViewModel by viewModels { ScrapDetailFactory() }
     private val binding by lazy { ActivityScrapDetailBinding.inflate(layoutInflater) }
 
     private val model by lazy {
@@ -36,8 +41,42 @@ class ScrapDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        Log.d("ssss","즐겨찾기 버튼 상태 :${model!!.isLike}")
         scrapWebView()
+        initView()
+        initViewModel()
+        viewLike(model!!)
+        likeBtn()
+    }
+    private fun likeBtn() {
+        binding.scrapDetailLikeBtn.setOnClickListener {
+            with(scrapDetailViewModel) {
+
+                val uid = SharedPreferences.getUid(this@ScrapDetail)
+                if (model!!.isLike) {
+                    binding.scrapDetailLikeBtn.setImageResource(R.drawable.hollowstar)
+                    removeFirebaseScrapData(uid, model!!)
+                } else {
+                    binding.scrapDetailLikeBtn.setImageResource(R.drawable.paintedstar)
+                }
+            }
+        }
+    }
+
+    private fun viewLike(model:ScrapEntity) {
+        if(model.isLike){
+            binding.scrapDetailLikeBtn.setImageResource(R.drawable.paintedstar)
+        } else binding.scrapDetailLikeBtn.setImageResource(R.drawable.hollowstar)
+    }
+
+    private fun initView() {
+//        scrapDetailViewModel.getFirebaseScrapData(SharedPreferences.getUid(this))
+    }
+    private fun initViewModel() {
+        with(scrapDetailViewModel) {
+            scrapDetailResults.observe(this@ScrapDetail) {
+            }
+        }
     }
 
     private fun scrapWebView() {
