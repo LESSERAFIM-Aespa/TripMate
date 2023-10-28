06a4e9f6db6c687b2440758e2860c0e75fc2cb73
seongssu <loveforggong@gmail.com>
2023-10-29 08:55:10 +0900

    [Refector] viewmodel 리팩토링

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileBoardViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileBoardViewModel.kt
@@ -4,30 +4,31 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
-import kr.sparta.tripmate.domain.model.firebase.toCommunity
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseBoardDataFromBoardRepo
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveBoardFirebase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateCommuIsViewFromBoardRepo
+import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseBoardDataUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveFirebaseBoardDataUseCase
 
 class UserProfileBoardViewModel(
-    private val getFirebaseBoardDataFromBoardRepo:
-    GetFirebaseBoardDataFromBoardRepo,
-    private val saveBoardFirebase: SaveBoardFirebase
+    private val getFirebaseBoardDataUseCase:
+    GetFirebaseBoardDataUseCase,
+    private val saveFirebaseBoardDataUseCase: SaveFirebaseBoardDataUseCase
 ) : ViewModel() {
     private val _userPage: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
     val userPage get() = _userPage
+    private val _likeKeyResults: MutableLiveData<List<KeyModelEntity?>> = MutableLiveData()
+    val likeKeyResults get() = _likeKeyResults
 
-    fun getFirebaseBoardData() {
+    fun getFirebaseBoardData(uid:String) {
         Log.d("asdfasdfasdf", "뷰모델 데이터 호출은 되고있냐?")
-        getFirebaseBoardDataFromBoardRepo.invoke(_userPage)
+        getFirebaseBoardDataUseCase.invoke(uid,_userPage,_likeKeyResults)
     }
 
-    fun updateView(model:CommunityModelEntity){
+    fun updateView(uid:String,model:CommunityModelEntity){
         Log.d("asdfasdfasdf", "뷰모델 조회수 호출은 되고있냐?")
         val currentView = model.views?.toIntOrNull() ?:0
         val newViews = currentView + 1
         model.views = newViews.toString()
-        saveBoardFirebase.invoke(model, _userPage)
-        getFirebaseBoardData()
+        saveFirebaseBoardDataUseCase.invoke(model, _userPage)
+        getFirebaseBoardData(uid)
     }
 }
