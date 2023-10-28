c87885f91aca07dc88f411d4c5faa27823ee5166
SeoJungHan <seojh5939@gmail.com>
2023-10-29 01:18:11 +0900

    [Remove]: 파일 삭제

--- app/src/main/java/kr/sparta/tripmate/data/model/community/KeyModel.kt
+++
@@ -1,9 +0,0 @@
-package kr.sparta.tripmate.data.model.community
-
-data class KeyModel(
-    val uid: String?,
-    val key: String?,
-    var myCommuIsLike: Boolean
-) {
-    constructor() : this(null, null, false)
-}
