4b2c2b3e0bfd5d911e62615cea244d50a497fe52
seongssu <loveforggong@gmail.com>
2023-10-28 11:40:38 +0900

    [Fix] userProfile 게시판 : 아이템 클릭 시 게시판 디테일로 이동, 화면에 데이터 표시, 조회수증가& 이미지 뒤섞임& 무한증식현상 해결

--- app/src/main/java/kr/sparta/tripmate/ui/userprofile/board/UserProfileBoardFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/userprofile/board/UserProfileBoardFragment.kt
@@ -1,26 +1,52 @@
 package kr.sparta.tripmate.ui.userprofile.board
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.sparta.tripmate.databinding.FragmentUserProfileBoardBinding
+import kr.sparta.tripmate.ui.community.CommunityDetailActivity
+import kr.sparta.tripmate.ui.viewmodel.userproflie.UserProfileBoardFactory
+import kr.sparta.tripmate.ui.viewmodel.userproflie.UserProfileBoardViewModel
+import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
 
 /**
  * 작성자: 서정한
  * 내용: 해당User가 작성한 글목록을 보여주는 Fragment
  * */
 class UserProfileBoardFragment : Fragment() {
-    companion object{
-        fun newInstance() : UserProfileBoardFragment = UserProfileBoardFragment()
+    companion object {
+        fun newInstance(): UserProfileBoardFragment = UserProfileBoardFragment()
     }
 
-    private var _binding : FragmentUserProfileBoardBinding? = null
+    private val userProfileBoardViewModel: UserProfileBoardViewModel by viewModels {
+        UserProfileBoardFactory()
+    }
+    private lateinit var boardContext: Context
+    private var _binding: FragmentUserProfileBoardBinding? = null
     private val binding get() = _binding!!
 
-    private val adapter by lazy {
-        UserProfileBoardListAdapter()
+    private val boardAdapter by lazy {
+        UserProfileBoardListAdapter(
+            onItemClicked = { model, position ->
+                userProfileBoardViewModel.updateView(model)
+                val intent =CommunityDetailActivity.newIntentForEntity(boardContext, model)
+                intent.putExtra("Data", model)
+                startActivity(intent)
+
+            }
+        )
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        boardContext = context
     }
 
     override fun onCreateView(
@@ -33,13 +59,43 @@ class UserProfileBoardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        callDataSource()
         initView()
+        initViewModel()
+
     }
 
-    private fun initView()=with(binding) {
-        userProfileBoardRecyclerview.adapter = adapter
+    private fun initViewModel() {
+        val uid = SharedPreferences.getUidFromUser(boardContext)
+        with(userProfileBoardViewModel) {
+            userPage.observe(viewLifecycleOwner) {getList ->
+                Log.d("asdfasdfasdf", "변화감지 ${getList[0]!!.views}")
+                Log.d("TripMates", "변화는 감지되는거야?")
+                val filteredList = getList.filter { it?.id == uid }
+                boardAdapter.submitList(filteredList)
+                boardAdapter.notifyDataSetChanged()
+            }
+        }
     }
+    private fun initView() = with(binding) {
+        userProfileBoardRecyclerview.apply {
+            adapter = boardAdapter
+            layoutManager = LinearLayoutManager(boardContext)
+            setHasFixedSize(true)
+        }
+    }
+
+    fun callDataSource() {
+        Log.d("asdfasdfasdf", "유저 uid : ${SharedPreferences.getUidFromUser(boardContext)}")
+        userProfileBoardViewModel.getFirebaseBoardData()
+    }
+
+//    override fun onResume() {
+//        super.onResume()
+//        userProfileBoardViewModel.getFirebaseBoardData(SharedPreferences.getUidFromUser(boardContext))
+//    }
+
+
 
     override fun onDestroy() {
         _binding = null
