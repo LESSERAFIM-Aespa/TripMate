21d4ffa7018eed49358af4fb40f4f62aca18e12e
PotatoMeme <kshk0897@gmail.com>
2023-10-30 09:26:26 +0900

    [Feat] BudgetStatistics PieData 적용확인

--- app/src/main/java/kr/sparta/tripmate/ui/budget/detail/statistics/BudgetDetailStatisticsFragment.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/budget/detail/statistics/BudgetDetailStatisticsFragment.kt
@@ -19,6 +19,10 @@ import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.ColorTemplate.COLORFUL_COLORS
 import com.github.mikephil.charting.utils.MPPointF
+import com.google.android.play.integrity.internal.m
+import kr.sparta.tripmate.data.model.budget.Budget
+import kr.sparta.tripmate.data.model.budget.Category
+import kr.sparta.tripmate.data.model.budget.Procedure
 import kr.sparta.tripmate.data.repository.BudgetRepositoryImpl
 import kr.sparta.tripmate.databinding.FragmentBudgetDetailStatisticsBinding
 import kr.sparta.tripmate.ui.viewmodel.budget.detail.statistics.BudgetStatisticsViewModel
@@ -66,17 +70,101 @@ class BudgetDetailStatisticsFragment : Fragment() {
     }
 
     private fun initViews() = with(binding) {
+        binding.budgetDetailExpenditurePiechart.apply {
+            description.isEnabled = false
+            isRotationEnabled = false
+            centerText = "지출"
 
+            setUsePercentValues(true)
+            setEntryLabelColor(Color.BLACK)
+            setTouchEnabled(false)
+        }
     }
 
     private fun initViewModels() {
-        with(viewModel){
-            budgetTotal.observe(viewLifecycleOwner){ budgetTotal ->
-                val budget = budgetTotal.first
-                val categories = budgetTotal.second
-                val procedueres = budgetTotal.third
+        with(viewModel) {
+            budgetTotal.observe(viewLifecycleOwner) { budgetTotal ->
+                val budget: Budget = budgetTotal.first
+                val categories: List<Category> = budgetTotal.second
+                val categoryMap: Map<Int, Category> =
+                    mutableMapOf<Int, Category>().apply { categories.forEach { put(it.num, it) } }
 
-                Log.d(TAG, "initViewModels:  $budget")
+                val procedures: List<Procedure> = budgetTotal.third
+                val procedureGroupByCategoryNum: Map<Int, List<Procedure>> =
+                    procedures.groupBy { it.categoryNum }
+
+                val totalDataMap : Map<Int,Int> = mutableMapOf<Int, Int>().apply {
+                    procedureGroupByCategoryNum.forEach { i, procedures ->
+                        put(i,procedures.sumOf { it.money })
+                    }
+                }
+                // 지출
+                val totalExpenditureData : MutableMap<Int,Int> = mutableMapOf()
+                // 수입
+                val totalIncomeData : MutableMap<Int,Int> = mutableMapOf()
+
+                totalDataMap.forEach { key, sum ->
+                    Log.d(TAG, "initViewModels: key,$key sum,$sum")
+                    when{
+                        sum < 0 -> totalIncomeData[key] = -sum
+                        sum > 0 -> totalExpenditureData[key] = sum
+                    }
+                }
+
+                Log.d(TAG, "initViewModels:totalExpenditureData.size, ${totalExpenditureData.size}")
+                Log.d(TAG, "initViewModels:totalIncomeData.size, ${totalIncomeData.size}")
+
+                if (totalExpenditureData.isNotEmpty()){
+                    val entries = ArrayList<PieEntry>()
+                    val colorsItems = ArrayList<Int>()
+
+                    totalExpenditureData.forEach { key, sum ->
+                        entries.add(PieEntry(sum.toFloat(),categoryMap[key]?.name))
+                        Log.d(TAG, "initViewModels: currentEntry ${entries.last().value} ${entries.last().label}")
+                        colorsItems.add(Color.parseColor(categoryMap[key]?.color))
+                    }
+
+
+                    Log.d(TAG, "initViewModels:entries.size, ${entries.size}")
+                    Log.d(TAG, "initViewModels:colorsItems.size, ${colorsItems.size}")
+
+                    val pieDataSet = PieDataSet(entries, "")
+                    pieDataSet.apply {
+                        colors = colorsItems
+                        valueTextColor = Color.BLACK
+                        valueTextSize = 16f
+                    }
+                    val pieData = PieData(pieDataSet)
+                    binding.budgetDetailExpenditurePiechart.data = pieData
+                    binding.budgetDetailExpenditurePiechart.data.setValueFormatter(PercentFormatter())
+                    binding.budgetDetailExpenditurePiechart.invalidate()
+                }
+
+                if (totalIncomeData.isNotEmpty()){
+                    val entries = ArrayList<PieEntry>()
+                    val colorsItems = ArrayList<Int>()
+
+                    totalIncomeData.forEach { key, sum ->
+                        entries.add(PieEntry(sum.toFloat(),categoryMap[key]?.name))
+                        Log.d(TAG, "initViewModels: currentEntry ${entries.last().value} ${entries.last().label}")
+                        colorsItems.add(Color.parseColor(categoryMap[key]?.color))
+                    }
+
+
+                    Log.d(TAG, "initViewModels:entries.size, ${entries.size}")
+                    Log.d(TAG, "initViewModels:colorsItems.size, ${colorsItems.size}")
+
+                    val pieDataSet = PieDataSet(entries, "")
+                    pieDataSet.apply {
+                        colors = colorsItems
+                        valueTextColor = Color.BLACK
+                        valueTextSize = 16f
+                    }
+                    val pieData = PieData(pieDataSet)
+                   /* binding.budgetDetailSpendPiechart.data = pieData
+                    binding.budgetDetailSpendPiechart.data.setValueFormatter(PercentFormatter())
+                    binding.budgetDetailSpendPiechart.invalidate()*/
+                }
             }
         }
     }
