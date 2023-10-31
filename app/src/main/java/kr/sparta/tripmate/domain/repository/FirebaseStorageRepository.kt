d412ab728db9d5ad9ba77540136d3ec5c0028a8f
SeoJungHan <seojh5939@gmail.com>
2023-10-31 11:52:08 +0900

    [Refector] : FirebaseStorage Data Layer

--- app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseStorageRepository.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseStorageRepository.kt
@@ -3,6 +3,10 @@ package kr.sparta.tripmate.domain.repository
 import android.graphics.Bitmap
 import io.reactivex.rxjava3.subjects.PublishSubject
 
+/**
+ * 작성자: 서정한
+ * 내용: FirebaseStorage Repository
+ * */
 interface FirebaseStorageRepository {
     fun uploadImage(
         imgName: String,
