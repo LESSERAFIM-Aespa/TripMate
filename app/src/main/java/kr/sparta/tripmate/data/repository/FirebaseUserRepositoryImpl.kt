03fcf85909df1fe4f4b85da62f1237c176865d65
SeoJungHan <seojh5939@gmail.com>
2023-10-31 11:50:57 +0900

    [Refector] : 유저 Data Layer

--- app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseUserRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/FirebaseUserRepositoryImpl.kt
@@ -2,11 +2,16 @@ package kr.sparta.tripmate.data.repository
 
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
-import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
+import kr.sparta.tripmate.data.datasource.remote.FirebaseBoardRemoteDataSource
+import kr.sparta.tripmate.data.datasource.remote.FirebaseUserRemoteDataSource
 import kr.sparta.tripmate.domain.model.login.UserDataEntity
 import kr.sparta.tripmate.domain.repository.FirebaseUserRepository
 
-class FirebaseUserRepositoryImpl(private val remoteSource: FirebaseDBRemoteDataSource) :
+/**
+ * 작성자: 서정한
+ * 내용: 유저 Repository
+ * */
+class FirebaseUserRepositoryImpl(private val remoteSource: FirebaseUserRemoteDataSource) :
     FirebaseUserRepository {
     override fun updateUserData(uid: String, userLiveData: MutableLiveData<UserDataEntity?>) {
         remoteSource.updateUserData(uid, userLiveData)
--- app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseUserRepository.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/repository/FirebaseUserRepository.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import kr.sparta.tripmate.domain.model.login.UserDataEntity
 
+/**
+ * 작성자: 서정한
+ * 내용: 유저 Repository
+ * */
 interface FirebaseUserRepository {
     fun updateUserData(
         uid: String, userLiveData: MutableLiveData<UserDataEntity?>
