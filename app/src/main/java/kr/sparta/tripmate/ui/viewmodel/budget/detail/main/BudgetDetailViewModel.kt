47273f62122988ec5338d01aeb47971aa61bbdfc
PotatoMeme <kshk0897@gmail.com>
2023-11-01 09:35:40 +0900

    [Feat] BudgetDetail toolbar title 최신화

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/detail/main/BudgetDetailViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/detail/main/BudgetDetailViewModel.kt
@@ -1,8 +1,10 @@
 package kr.sparta.tripmate.ui.viewmodel.budget.detail.main
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import kr.sparta.tripmate.data.model.budget.Budget
@@ -15,7 +17,8 @@ import kr.sparta.tripmate.ui.budget.detail.procedure.ProcedureModel
  * 작성자: 서정한
  * 내용: 가계부 과정Fragment의 ViewModel
  * */
-class BudgetDetailViewModel(private val repository: BudgetRepository) : ViewModel() {
+class BudgetDetailViewModel(private val repository: BudgetRepository,private val budgetNum : Int) : ViewModel() {
+    val budgetLiveData : LiveData<Budget> = repository.getBugetToFlowWhenBudgetChangedWithNum(budgetNum).asLiveData()
     /**
      * 작성자: 서정한
      * 내용: 현재 가계부를 삭제합니다.
