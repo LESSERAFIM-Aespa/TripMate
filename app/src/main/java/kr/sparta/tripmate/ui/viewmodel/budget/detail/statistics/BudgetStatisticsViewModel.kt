182993be1906c764609f4a3833d531db7e64e7f4
PotatoMeme <kshk0897@gmail.com>
2023-11-01 16:11:44 +0900

    [Fix] BudgetDetail 가계부 수정시 과정리스트의 원금등 잔액 최신화 안되던것 수정

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/detail/statistics/BudgetStatisticsViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/detail/statistics/BudgetStatisticsViewModel.kt
@@ -1,13 +1,58 @@
 package kr.sparta.tripmate.ui.viewmodel.budget.detail.statistics
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.google.android.play.integrity.internal.c
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kr.sparta.tripmate.data.model.budget.Budget
 import kr.sparta.tripmate.data.model.budget.Category
 import kr.sparta.tripmate.data.model.budget.Procedure
+import kr.sparta.tripmate.data.model.budget.toModel
 import kr.sparta.tripmate.domain.repository.BudgetRepository
+import kr.sparta.tripmate.ui.budget.detail.procedure.ProcedureModel
 
-class BudgetStatisticsViewModel(budgetNum:Int,repository: BudgetRepository) : ViewModel() {
-    val budgetTotal : LiveData<Triple<Budget,List<Category>,List<Procedure>>> = repository.getBudgetTotalToFlowWhenProccessChangedWithBudgetNum(budgetNum).asLiveData()
+class BudgetStatisticsViewModel(budgetNum: Int, repository: BudgetRepository) : ViewModel() {
+    private val _budgetLiveData: MutableLiveData<Budget> = MutableLiveData()
+    val budgetLiveData get() = _budgetLiveData
+
+    private val _procedureList: MutableLiveData<List<ProcedureModel>> = MutableLiveData()
+    val procedureList get() = _procedureList
+
+    val budgetTotal: LiveData<Triple<Budget, List<Category>, List<Procedure>>> =
+        flow {
+            repository.getBudgetTotalToFlowWhenProccessChangedWithBudgetNum(budgetNum)
+                .collect { triple ->
+                    val budget = triple.first
+                    val categories = triple.second
+                    val procedures = triple.third
+
+                    val categoriesMap: Map<Int, Category> = mutableMapOf<Int, Category>().apply {
+                        categories.forEach {
+                            put(it.num, it)
+                        }
+                    }
+
+                    var currentMoney = budget.money
+                    fun reduceCurrentMoney(reduce: Int): Int {
+                        currentMoney -= reduce
+                        return currentMoney
+                    }
+
+                    val postList: List<ProcedureModel> = procedures.map {
+                        it.toModel().copy(
+                            beforeMoney = currentMoney,
+                            totalAmount = reduceCurrentMoney(it.money),
+                            categoryColor = categoriesMap[it.categoryNum]?.color ?: "#f8f8f8",
+                            categoryName = categoriesMap[it.categoryNum]?.name ?: "",
+                        )
+                    }
+                    _budgetLiveData.postValue(budget)
+                    _procedureList.postValue(postList)
+
+                    emit(triple)
+                }
+        }.asLiveData()
 }
