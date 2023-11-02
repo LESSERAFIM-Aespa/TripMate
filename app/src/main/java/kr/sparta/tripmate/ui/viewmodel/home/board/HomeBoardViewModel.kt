8aaa1c93df1f71818a79069f5c8a19a434e9d03d
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:04:12 +0900

    [Refector]: 홈 인기 ViewModel

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardViewModel.kt
@@ -2,31 +2,38 @@ package kr.sparta.tripmate.ui.viewmodel.home.board
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kr.sparta.tripmate.domain.model.firebase.CommunityModelEntity
-import kr.sparta.tripmate.domain.model.firebase.KeyModelEntity
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseBoardDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveFirebaseBoardDataUseCase
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.sparta.tripmate.domain.model.community.CommunityEntity
+import kr.sparta.tripmate.domain.model.community.toEntity
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetAllBoardsUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardLikeUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardViewsUseCase
 
 class HomeBoardViewModel(
-    private val getFirebaseBoardDataUseCase: GetFirebaseBoardDataUseCase,
-    private val saveFirebaseBoardDataUseCase: SaveFirebaseBoardDataUseCase
+    private val getAllBoardsUseCase: GetAllBoardsUseCase,
+    private val updateBoardViewsUseCase: UpdateBoardViewsUseCase,
 ) :
     ViewModel
         () {
-    private val _homeBoard: MutableLiveData<List<CommunityModelEntity?>> = MutableLiveData()
-    val homeBoard get() = _homeBoard
-    private val _likeKeyResults: MutableLiveData<List<KeyModelEntity?>> = MutableLiveData()
-    val likeKeyResults get() = _likeKeyResults
+    private val _boards: MutableLiveData<List<CommunityEntity?>> = MutableLiveData()
+    val boards get() = _boards
 
-    fun getHomeBoardData(uid: String) {
-        getFirebaseBoardDataUseCase.invoke(uid, _homeBoard, _likeKeyResults)
+    /**
+     * 작성자: 서정한
+     * 내용: 모든 게시글목록을 가져옵니다.
+     * */
+    fun getAllBoards() = viewModelScope.launch {
+        getAllBoardsUseCase().collect() {
+            _boards.value = it.toEntity()
+        }
     }
 
-    fun updateBoardView(model: CommunityModelEntity) {
-        val currentView = model.views?.toIntOrNull() ?: 0
-        val newViews = currentView + 1
-        model.views = newViews.toString()
-        saveFirebaseBoardDataUseCase.invoke(model, _homeBoard)
-        getHomeBoardData(model.id)
-    }
+    /**
+     * 작성자: 서정한
+     * 내용: 게시글 조회수 증가
+     * */
+    fun updateBoardView(model: CommunityEntity) = updateBoardViewsUseCase(model)
 }
