9bf262bb62696ca309646cbb089779b2e701b941
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:04:12 +0900

    [Refector]: 홈 인기 ViewModel

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/board/HomeBoardFactory.kt
@@ -2,24 +2,25 @@ package kr.sparta.tripmate.ui.viewmodel.home.board
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
-import kr.sparta.tripmate.data.repository.FirebaseBoardRepositoryImpl
-import kr.sparta.tripmate.domain.repository.FirebaseBoardRepository
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseBoardDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveFirebaseBoardDataUseCase
+import kr.sparta.tripmate.data.datasource.remote.community.FirebaseBoardRemoteDataSource
+import kr.sparta.tripmate.data.repository.community.FirebaseBoardRepositoryImpl
+import kr.sparta.tripmate.domain.repository.community.FirebaseBoardRepository
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetAllBoardsUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardLikeUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardViewsUseCase
 
 class HomeBoardFactory : ViewModelProvider.Factory {
     private val repository: FirebaseBoardRepository by lazy {
         FirebaseBoardRepositoryImpl(
-            FirebaseDBRemoteDataSource()
+            FirebaseBoardRemoteDataSource()
         )
     }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeBoardViewModel::class.java)) {
             return HomeBoardViewModel(
-                GetFirebaseBoardDataUseCase(repository),
-                SaveFirebaseBoardDataUseCase(repository)
+                GetAllBoardsUseCase(repository),
+                UpdateBoardViewsUseCase(repository),
             ) as T
         }
         throw IllegalArgumentException("에러")
