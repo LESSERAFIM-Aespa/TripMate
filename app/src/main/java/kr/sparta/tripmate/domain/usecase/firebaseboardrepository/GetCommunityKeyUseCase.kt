8f5af388a469018c183ad88e806e150d4bfcedf3
SeoJungHan <seojh5939@gmail.com>
2023-11-03 00:56:25 +0900

    [Refector]: 커뮤니티 관련 UseCase

--- app/src/main/java/kr/sparta/tripmate/domain/usecase/firebaseboardrepository/GetCommunityKeyUseCase.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/firebaseboardrepository/GetCommunityKeyUseCase.kt
@@ -1,11 +1,11 @@
 package kr.sparta.tripmate.domain.usecase.firebaseboardrepository
 
-import kr.sparta.tripmate.domain.repository.FirebaseBoardRepository
+import kr.sparta.tripmate.domain.repository.community.FirebaseBoardRepository
 
 /**
  * 작성자: 서정한
  * 내용: RDB에서 key값 가져오기
  * */
 class GetCommunityKeyUseCase(private val repository: FirebaseBoardRepository) {
-    operator fun invoke(): String = repository.getCommunityKey()
+    operator fun invoke(): String = repository.getKey()
 }
