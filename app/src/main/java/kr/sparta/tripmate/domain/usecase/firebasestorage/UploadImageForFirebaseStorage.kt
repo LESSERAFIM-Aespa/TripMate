6d676accbf452264ff04f6c1bee747fd9a38dd60
SeoJungHan <seojh5939@gmail.com>
2023-11-03 00:58:23 +0900

    [Refector]: 커뮤니티 게시글 이미지업로드 UseCase

--- app/src/main/java/kr/sparta/tripmate/domain/usecase/firebasestorage/UploadImageForFirebaseStorage.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/firebasestorage/UploadImageForFirebaseStorage.kt
@@ -1,17 +1,14 @@
 package kr.sparta.tripmate.domain.usecase.firebasestorage
 
 import android.graphics.Bitmap
-import io.reactivex.rxjava3.subjects.PublishSubject
-import kr.sparta.tripmate.domain.repository.FirebaseStorageRepository
+import kr.sparta.tripmate.domain.repository.community.FirebaseStorageRepository
 
 class UploadImageForFirebaseStorage(private val repository: FirebaseStorageRepository) {
-    operator fun invoke(
+    suspend operator fun invoke(
         imgName: String,
         image: Bitmap,
-        publishSubject: PublishSubject<String>
-    ) = repository.uploadImage(
+    ): String = repository.uploadImage(
         imgName = imgName,
         image = image,
-        publishUri = publishSubject,
     )
 }
