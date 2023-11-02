24ff94b7af9d6d4bcd6664443a6cdee2f9cab210
ssu <loveforggong@gmail.com>
2023-11-02 19:35:19 +0900

    test

--- app/src/main/java/kr/sparta/tripmate/ui/login/LoginActivity.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/login/LoginActivity.kt
@@ -48,7 +48,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-val x = 1
+
         auth = FirebaseAuth.getInstance()
         login_Database = Firebase.database.reference
 
