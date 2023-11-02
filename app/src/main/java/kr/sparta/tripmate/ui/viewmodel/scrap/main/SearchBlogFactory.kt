db89f3200570ee7ad59040a82a46d96626b7baa7
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:07:02 +0900

    [Refector]: 블로그 검색페이지 ViewModel

---
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/scrap/main/SearchBlogFactory.kt
@@ -0,0 +1,34 @@
+package kr.sparta.tripmate.ui.viewmodel.scrap.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.sparta.tripmate.api.naver.NaverNetWorkClient
+import kr.sparta.tripmate.data.datasource.remote.community.scrap.FirebaseBlogScrapRemoteDataSource
+import kr.sparta.tripmate.data.repository.community.FirebaseBlogScrapRepositoryImpl
+import kr.sparta.tripmate.data.repository.search.SearchRepositoryImpl
+import kr.sparta.tripmate.domain.repository.community.FirebaseBlogScrapRepository
+import kr.sparta.tripmate.domain.repository.search.SearchRepository
+import kr.sparta.tripmate.domain.usecase.GetSearchBlogUseCase
+import kr.sparta.tripmate.domain.usecase.firebasescraprepository.GetAllBlogScrapsUseCase
+import kr.sparta.tripmate.domain.usecase.firebasescraprepository.UpdateBlogScrapUseCase
+
+class SearchBlogFactory : ViewModelProvider.Factory {
+    private val searchRepository: SearchRepository = SearchRepositoryImpl(
+        NaverNetWorkClient.apiService
+    )
+    private val blogScrapRepository: FirebaseBlogScrapRepository = FirebaseBlogScrapRepositoryImpl(
+        FirebaseBlogScrapRemoteDataSource()
+    )
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SearchBlogViewModel::class.java)) {
+            return SearchBlogViewModel(
+                GetSearchBlogUseCase(searchRepository),
+                UpdateBlogScrapUseCase(blogScrapRepository),
+                GetAllBlogScrapsUseCase(blogScrapRepository),
+            ) as T
+        } else {
+            throw IllegalArgumentException("Not found ViewModel class.")
+        }
+    }
+}
