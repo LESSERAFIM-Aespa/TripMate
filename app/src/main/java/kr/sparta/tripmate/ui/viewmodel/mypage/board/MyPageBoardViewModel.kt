19a7a95506a2080710478f2b8f6b1efd46fbdd66
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:06:11 +0900

    [Refector]: 내정보페이지 내글목록 ViewModel

---
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/mypage/board/MyPageBoardViewModel.kt
@@ -0,0 +1,50 @@
+package kr.sparta.tripmate.ui.viewmodel.mypage.board
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.sparta.tripmate.domain.model.community.CommunityEntity
+import kr.sparta.tripmate.domain.model.community.toEntity
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetAllBoardsUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardLikeUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardViewsUseCase
+
+class MyPageBoardViewModel(
+    private val getAllBoardsUseCase: GetAllBoardsUseCase,
+    private val updateBoardViewsUseCase: UpdateBoardViewsUseCase,
+    private val updateBoardLikeUseCase: UpdateBoardLikeUseCase,
+) :
+    ViewModel() {
+    private val _myBoards: MutableLiveData<List<CommunityEntity?>> = MutableLiveData()
+    val myBoards get() = _myBoards
+
+    /**
+     * 작성자: 서정한
+     * 내용: 모든 게시글 목록을 가져옵니다.
+     * */
+    fun getAllMyBoards(uid: String)= CoroutineScope(Dispatchers.Main).launch {
+        getAllBoardsUseCase().collect() { boards ->
+            val myBoard = boards.filter { it?.id == uid }
+
+            _myBoards.value = myBoard.toEntity()
+        }
+    }
+
+    /**
+     * 작성자: 서정한
+     * 내용: 조회수 증가
+     * */
+    fun updateBoardView(model: CommunityEntity) {
+        updateBoardViewsUseCase(model)
+    }
+
+    /**
+     * 작성자: 서정한
+     * 내용: 게시글 좋아요 업데이트
+     * */
+    fun updateBoardLikes(uid: String, key: String) {
+        updateBoardLikeUseCase(uid, key)
+    }
+}
