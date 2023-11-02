716fd8a4af8973d90bd072405adaea73ce9e9b70
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:07:31 +0900

    [Refector]: 유저정보페이 ViewModel

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileBoardFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileBoardFactory.kt
@@ -2,25 +2,24 @@ package kr.sparta.tripmate.ui.viewmodel.userproflie
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
-import kr.sparta.tripmate.data.repository.FirebaseBoardRepositoryImpl
-import kr.sparta.tripmate.domain.repository.FirebaseBoardRepository
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseBoardDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetFirebaseLikeDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveFirebaseBoardDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.SaveFirebaseLikeDataUseCase
+import kr.sparta.tripmate.data.datasource.remote.community.FirebaseBoardRemoteDataSource
+import kr.sparta.tripmate.data.repository.community.FirebaseBoardRepositoryImpl
+import kr.sparta.tripmate.domain.repository.community.FirebaseBoardRepository
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetAllBoardsUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardLikeUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateBoardViewsUseCase
 
 class UserProfileBoardFactory : ViewModelProvider.Factory {
     private val repository: FirebaseBoardRepository by lazy {
-        FirebaseBoardRepositoryImpl(FirebaseDBRemoteDataSource())
+        FirebaseBoardRepositoryImpl(FirebaseBoardRemoteDataSource())
     }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(UserProfileBoardViewModel::class.java)) {
             return UserProfileBoardViewModel(
-                GetFirebaseBoardDataUseCase(repository),
-                SaveFirebaseBoardDataUseCase(repository),
-                SaveFirebaseLikeDataUseCase(repository)
+                GetAllBoardsUseCase(repository),
+                UpdateBoardViewsUseCase(repository),
+                UpdateBoardLikeUseCase(repository)
             ) as T
         }
         throw IllegalArgumentException("에러")
