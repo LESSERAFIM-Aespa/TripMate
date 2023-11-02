fc926ad4e77b3c55875499fc91ca537732abf322
SeoJungHan <seojh5939@gmail.com>
2023-11-03 01:03:55 +0900

    [Refector]: 커뮤니티 글쓰기페이 ViewModel

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/write/CommunityWriteFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/community/write/CommunityWriteFactory.kt
@@ -2,20 +2,20 @@ package kr.sparta.tripmate.ui.viewmodel.community.write
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.data.datasource.remote.FirebaseDBRemoteDataSource
-import kr.sparta.tripmate.data.datasource.remote.FirebaseStorageRemoteDataSource
-import kr.sparta.tripmate.data.repository.FirebaseBoardRepositoryImpl
-import kr.sparta.tripmate.data.repository.FirebaseStorageRepositoryImpl
-import kr.sparta.tripmate.domain.repository.FirebaseBoardRepository
-import kr.sparta.tripmate.domain.repository.FirebaseStorageRepository
+import kr.sparta.tripmate.data.datasource.remote.community.FirebaseBoardRemoteDataSource
+import kr.sparta.tripmate.data.datasource.remote.community.FirebaseStorageRemoteDataSource
+import kr.sparta.tripmate.data.repository.community.FirebaseBoardRepositoryImpl
+import kr.sparta.tripmate.data.repository.community.FirebaseStorageRepositoryImpl
+import kr.sparta.tripmate.domain.repository.community.FirebaseBoardRepository
+import kr.sparta.tripmate.domain.repository.community.FirebaseStorageRepository
+import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.AddBoardUseCase
 import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.GetCommunityKeyUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseboardrepository.UpdateCommunityWriteDataUseCase
 import kr.sparta.tripmate.domain.usecase.firebasestorage.UploadImageForFirebaseStorage
 
 class CommunityWriteFactory : ViewModelProvider.Factory {
     // RDB 레퍼지토리
-    private val databaseRepository: FirebaseBoardRepository by lazy {
-        FirebaseBoardRepositoryImpl(FirebaseDBRemoteDataSource())
+    private val boardRepository: FirebaseBoardRepository by lazy {
+        FirebaseBoardRepositoryImpl(FirebaseBoardRemoteDataSource())
     }
 
     // Storage 레퍼지토리
@@ -26,9 +26,9 @@ class CommunityWriteFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CommunityWriteViewModel::class.java)) {
             return CommunityWriteViewModel(
-                UpdateCommunityWriteDataUseCase(databaseRepository),
+                AddBoardUseCase(boardRepository),
                 UploadImageForFirebaseStorage(storageRepository),
-                GetCommunityKeyUseCase(databaseRepository),
+                GetCommunityKeyUseCase(boardRepository),
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class")
