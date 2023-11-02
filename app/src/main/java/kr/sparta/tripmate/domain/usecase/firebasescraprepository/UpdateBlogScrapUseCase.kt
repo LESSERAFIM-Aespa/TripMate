53999fc6f7e0182d4a8d81df0d334547cefe93ec
SeoJungHan <seojh5939@gmail.com>
2023-11-03 00:57:52 +0900

    [Refector]: 블로그스크랩 UseCase

---
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/firebasescraprepository/UpdateBlogScrapUseCase.kt
@@ -0,0 +1,15 @@
+package kr.sparta.tripmate.domain.usecase.firebasescraprepository
+
+import kr.sparta.tripmate.domain.model.search.SearchBlogEntity
+import kr.sparta.tripmate.domain.repository.community.FirebaseBlogScrapRepository
+
+/**
+ * 작성자: 서정한
+ * 내용: 스크랩한 블로그 목록을 업데이트합니다.
+ * 블로그 목록에 없는 item일 경우 추가하고
+ * 있는경우 제거합니다.
+ * */
+class UpdateBlogScrapUseCase(private val repository: FirebaseBlogScrapRepository) {
+    operator fun invoke(uid: String, model: SearchBlogEntity) =
+        repository.updateBlogScrap(uid, model)
+}
