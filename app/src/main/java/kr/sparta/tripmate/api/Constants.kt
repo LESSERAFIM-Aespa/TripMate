5ab111bd340edb68c8c767f446a5c5bf4dbfeccc
monsterKyu <ilkyu092@naver.com>
2023-11-06 10:45:54 +0900

    <design> 블로그 추천 이미지 변경

--- app/src/main/java/kr/sparta/tripmate/api/Constants.kt
+++ app/src/main/java/kr/sparta/tripmate/api/Constants.kt
@@ -2,8 +2,8 @@ package kr.sparta.tripmate.api
 
 object Constants {
     val naverUrl = "https://openapi.naver.com/v1/"
-    const val client_id = ""
-    const val client_secret = ""
+    const val client_id = "MFOPiSjf4r_URaa3uuoS"
+    const val client_secret = "i1gWAQ88Dv"
 
     var EMPTYTYPE = 0
     var SCRAPTYPE = 1
