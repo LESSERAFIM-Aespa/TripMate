ffd446408940d6ebd04facb198127d9cffce5b20
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:39:16 +0900

    [Refactor] GetProceduresWithNumUseCase 생성

---
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/procedurerepository/GetProceduresWithNumUseCase.kt
@@ -0,0 +1,9 @@
+package kr.sparta.tripmate.domain.usecase.procedurerepository
+
+import kr.sparta.tripmate.data.model.budget.Procedure
+import kr.sparta.tripmate.domain.repository.budget.ProcedureRepository
+
+class GetProceduresWithNumUseCase(private val repository: ProcedureRepository) {
+    suspend operator fun invoke(num: Int): List<Procedure> =
+        repository.getProceduresWithNum(num)
+}
