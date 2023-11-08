4b0805ddd585415346ca44fccfd018b655a7e3a9
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:24:08 +0900

    [Refactor] BudgetRepositoryImpl DI 주입

--- app/src/main/java/kr/sparta/tripmate/data/repository/budget/BudgetRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/budget/BudgetRepositoryImpl.kt
@@ -1,184 +1,64 @@
 package kr.sparta.tripmate.data.repository.budget
 
-import android.content.Context
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kr.sparta.tripmate.data.datasource.local.budget.BudgetLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.CategoryProceduresLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.ProcedureLocalDataSource
 import kr.sparta.tripmate.data.model.budget.Budget
-import kr.sparta.tripmate.data.model.budget.Category
-import kr.sparta.tripmate.data.model.budget.Procedure
-import kr.sparta.tripmate.data.room.BudgetDatabase
 import kr.sparta.tripmate.domain.repository.budget.BudgetRepository
 
-class BudgetRepositoryImpl(context: Context) : BudgetRepository {
-    private val database = BudgetDatabase.getDatabsae(context)
-
-    private val budgetDao = database.getBudgetDao()
-    private val categoryDao = database.getCategoryDao()
-    private val proceduresDao = database.getProcedureDao()
-    private val budgetCategoriesDao = database.getBudgetCategoriesDao()
-    private val categoryProceduresDao = database.getCategoryProceduresDao()
-
+class BudgetRepositoryImpl(
+    private val budgetDataSource: BudgetLocalDataSource,
+    private val procedureDataSource: ProcedureLocalDataSource,
+    private val categoryProceduresDataSource: CategoryProceduresLocalDataSource,
+) : BudgetRepository {
     override suspend fun insertBudgets(vararg budgets: Budget) {
-        budgetDao.insertBudgets(*budgets)
+        budgetDataSource.insertBudgets(*budgets)
     }
 
     override suspend fun updateBudgets(vararg budgets: Budget) {
-        budgetDao.updateBudgets(*budgets)
+        budgetDataSource.updateBudgets(*budgets)
     }
 
     override suspend fun deleteBugets(vararg budgets: Budget) {
-        budgetDao.deleteBudgets(*budgets)
+        budgetDataSource.delteBudgets(*budgets)
     }
 
-    override suspend fun insertCategories(vararg categories: Category) {
-        categoryDao.insertCategories(*categories)
-    }
-
-    override suspend fun updateCategories(vararg categories: Category) {
-        categoryDao.updateCategories(*categories)
-    }
-
-    override suspend fun deleteCategories(vararg categories: Category) {
-        categoryDao.deleteCategories(*categories)
-    }
-
-    override suspend fun insertProcedures(vararg procedures: Procedure) {
-        proceduresDao.insertProcedures(*procedures)
-    }
-
-    override suspend fun updateProcedures(vararg procedures: Procedure) {
-        proceduresDao.updateProcedures(*procedures)
-    }
-
-    override suspend fun deleteProcedures(vararg procedures: Procedure) {
-        proceduresDao.deleteProcedures(*procedures)
-    }
-
-    /**
-     * 가계부(Budget)테이블의 모든값 Flow로 감싸반환,
-     * 해당값은 모두 resultMoeny를 가지고있음
-     * 가계부(Budget)테이블이 바뀌때마다 가져옴
-     * */
     override fun getAllBudgetsToFlowWhenBugetsChanged() = flow {
-        budgetDao.getAllBudgetsOrederByDateToFlow().collect {
-            val list = it.map {
+        budgetDataSource.getAllBudgetsOrederByDateToFlow().collect { budgets ->
+            val list = budgets.map { budget ->
                 val categoryProceduresList =
-                    categoryProceduresDao.getAllCategoryProceduresWithBudgetNum(it.num)
-                val reduce = categoryProceduresList.sumOf {
-                    it.procedures.orEmpty().sumOf {
-                        it.money
-                    }
+                    categoryProceduresDataSource.getAllCategoryProceduresWithBudgetNum(budget.num)
+                val reduce = categoryProceduresList.sumOf { categoryProcedures ->
+                    categoryProcedures.procedures.orEmpty().sumOf { it.money }
                 }
-                it.resultMoeny = it.money - reduce
-                it
+                budget.resultMoeny = budget.money - reduce
+                budget
             }
             emit(list)
         }
     }
 
-    /**
-     * 가계부(Budget)테이블의 모든값 Flow로 감싸반환,
-     * 해당값은 모두 resultMoeny를 가지고있음
-     * 과정(Procedure)테이블이 바뀔때마다 가져옴
-     * */
     override fun getAllBugetsToFlowWhenProceduresChanged() = flow {
-        proceduresDao.getAllProceduresToFlow().collect {
-            val list = budgetDao.getAllBudgetsOrederByDate().map {
+        procedureDataSource.getAllProceduresToFlow().collect {
+            val list = budgetDataSource.getAllBudgetsOrederByDate().map { budget ->
                 val categoryProceduresList =
-                    categoryProceduresDao.getAllCategoryProceduresWithBudgetNum(it.num)
-                val reduce = categoryProceduresList.sumOf {
-                    it.procedures.orEmpty().sumOf { it.money }
+                    categoryProceduresDataSource.getAllCategoryProceduresWithBudgetNum(budget.num)
+                val reduce = categoryProceduresList.sumOf { categoryProcedures ->
+                    categoryProcedures.procedures.orEmpty().sumOf { it.money }
                 }
-                it.resultMoeny = it.money - reduce
-                it
+                budget.resultMoeny = budget.money - reduce
+                budget
             }
             emit(list)
         }
     }
 
-    /**
-     * 해당키값을 가지는 Budget을 Flow로 감싸 반환
-     * 가계부(Budget)테이블이 바뀌때마다 가져옴
-     * */
     override fun getBugetToFlowWhenBudgetChangedWithNum(num: Int) = flow {
-        budgetDao.getAllBudgetsToFlowWithNum(num).collect {
+        budgetDataSource.getAllBudgetsToFlowWithNum(num).collect {
             if (it.isNotEmpty()) emit(it.first())
         }
     }
 
-    /**
-     * 해당키값(num)을 부모로 가지는 모든 카테고리들을 가져옴
-     * 카테고리(Category)테이블이 바뀌때마다 가져옴
-     * */
-    override fun getCategoriesToFlowWhenCategoriesChanged(num: Int) = flow {
-        categoryDao.getAllCategoriesToFlowWithBudgetNum(num).collect {
-            emit(it)
-        }
-    }
-
-    /**
-     * 최종적으로 해당값(num)을 부모로 가지는 과정들을 가져옴
-     * 과정(Procedure)테이블이 바뀌때마다 가져옴
-     * */
-//    override fun getAllProceduresToFlowWhenProccessChangedWithBudgetNum(num: Int) = flow {
-//        proceduresDao.getAllProceduresToFlow().collect {procedures ->
-//            val list = budgetCategoriesDao.getAllBudgetCategories(num).map {
-//                it.categories.orEmpty().map { it.num }
-//            }.reduce { acc, ints -> acc + ints }
-//            val result = proceduresDao.getAllProceduresOrderByTimeWithCategoryNums(list)
-//            emit(result)
-//        }
-//    }
-    override suspend fun getAllProceduresToFlowWhenProccessChangedWithBudgetNum(num: Int): List<Procedure> {
-        proceduresDao.getAllProcedures().let {
-            val list = budgetCategoriesDao.getAllBudgetCategories(num).map { categoryList ->
-                categoryList.categories.orEmpty().map { it.num }
-            }.reduce { acc, ints -> acc + ints }
-            val result = proceduresDao.getAllProceduresOrderByTimeWithCategoryNums(list)
-            return result
-        }
-    }
-
-    override suspend fun getBudgetCategories(num: Int) =
-        budgetCategoriesDao.getAllBudgetCategories(num)
-
-    override suspend fun getLastBudget(): List<Budget> = budgetDao.getLastBudget()
-    override suspend fun getProceduresWithNum(num: Int): List<Procedure> =
-        proceduresDao.getAllProceduresWithNum(num)
-
-    override suspend fun getPrcedouresWithCategoryNum(num: Int): List<Procedure> {
-        return proceduresDao.getAllProceduresWithCategoryNum(num)
-    }
-
-    override fun getProcedureToFlowWithNum(num: Int): Flow<List<Procedure>> =
-        proceduresDao.getAllProceduresWithNumToFlow(num)
-
-    override suspend fun getAllCategoriesWithBudgetNum(budgetNum: Int): List<Category> {
-        return categoryDao.getAllCategoriesWithBudgetNum(budgetNum)
-    }
-
-    override suspend fun getAllCategoriesForNum(num: Int): List<Category> =
-        categoryDao.getAllCategoriesWithNum(num)
-
-    override suspend fun getAllProceuduresWithCategoryNums(nums: List<Int>): List<Procedure> {
-        return proceduresDao.getAllProceduresWithCategoryNums(nums)
-    }
-
-    override fun getBudgetTotalToFlowWhenProccessChangedWithBudgetNum(num: Int): Flow<Triple<Budget, List<Category>, List<Procedure>>> =
-        flow {
-            proceduresDao.getAllProceduresToFlow().collect {
-                val triple = budgetCategoriesDao.getAllBudgetCategories(num)
-                if (triple.isNotEmpty()){
-                    val budgetCategories = triple.first()
-
-                    val budget = budgetCategories.budget
-                    val categories = budgetCategories.categories.orEmpty()
-                    val procedures =
-                        proceduresDao.getAllProceduresOrderByTimeWithCategoryNums(categories.map { it.num })
-
-                    emit(Triple(budget, categories, procedures))
-                }
-            }
-        }
-
+    override suspend fun getLastBudget(): List<Budget> = budgetDataSource.getLastBudget()
 }
