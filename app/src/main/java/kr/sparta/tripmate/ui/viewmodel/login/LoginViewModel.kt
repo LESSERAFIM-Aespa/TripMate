596a307a07a752c125cd8bda1ed531382d395dfb
ssu <loveforggong@gmail.com>
2023-11-09 02:13:39 +0900

    [feat] Login/ViewModel : 닉네임 데이터를 불러오는 UseCase와 유저데이터를 불러오는 UseCase를 추가했습니다.

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/login/LoginViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/login/LoginViewModel.kt
@@ -1,17 +1,28 @@
 package kr.sparta.tripmate.ui.viewmodel.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
 import kr.sparta.tripmate.domain.model.user.UserDataEntity
+import kr.sparta.tripmate.domain.model.user.toEntity
+import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.GetNickNameDataUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.GetUserDataUseCase
 import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.SaveUserDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.UpdateUserDataUseCase
 
 class LoginViewModel(
-    private val saveUserDataUseCase: SaveUserDataUseCase
+    private val saveUserDataUseCase: SaveUserDataUseCase,
+    private val getNickNameDataUseCase: GetNickNameDataUseCase,
+    private val getUserDataUseCase: GetUserDataUseCase
+
 ) :
     ViewModel() {
     private val auth = FirebaseAuth.getInstance()
+    private val _userDatas = MutableLiveData<UserDataEntity?>()
+    val userDatas : LiveData<UserDataEntity?> get() = _userDatas
 
     /**
      * 작성자: 서정한
@@ -25,4 +36,13 @@ class LoginViewModel(
      * 내용: Firebase RDB에 로그인한 유저정보를 저장함.
      * */
     fun saveCurrentUser(model: UserDataEntity) = saveUserDataUseCase(model)
+
+    suspend fun getNickNameData(nickName: String): Boolean =
+        getNickNameDataUseCase(nickName)
+
+   fun getUserData(uid: String) = viewModelScope.launch{
+       getUserDataUseCase(uid).collect() {
+           _userDatas.value = it.toEntity()
+       }
+    }
 }
