afd4eb30252ea54d16d70e39d3feb2def0807429
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:21:46 +0900

    [Refactor] ProcedureRepository DI 적용

---
+++ app/src/main/java/kr/sparta/tripmate/domain/repository/budget/ProcedureRepository.kt
@@ -0,0 +1,16 @@
+package kr.sparta.tripmate.domain.repository.budget
+
+import kotlinx.coroutines.flow.Flow
+import kr.sparta.tripmate.data.model.budget.Procedure
+
+interface ProcedureRepository {
+    suspend fun insertProcedures(vararg procedures: Procedure)
+    suspend fun updateProcedures(vararg procedures: Procedure)
+    suspend fun deleteProcedures(vararg procedures: Procedure)
+
+    suspend fun getAllProceduresWithBudgetNum(num: Int): List<Procedure>
+    suspend fun getProceduresWithNum(num: Int): List<Procedure>
+    suspend fun getProcedouresWithCategoryNum(num: Int): List<Procedure>
+    suspend fun getAllProceuduresWithCategoryNums(nums: List<Int>): List<Procedure>
+    fun getProcedureToFlowWithNum(num: Int): Flow<List<Procedure>>
+}
