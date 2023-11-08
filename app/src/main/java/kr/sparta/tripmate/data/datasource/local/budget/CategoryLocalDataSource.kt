3941e32b0e97e037a5addc688b779d46465fea9e
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:13:36 +0900

    [Refactor] CategoryLocalDataSource 추가

---
+++ app/src/main/java/kr/sparta/tripmate/data/datasource/local/budget/CategoryLocalDataSource.kt
@@ -0,0 +1,33 @@
+package kr.sparta.tripmate.data.datasource.local.budget
+
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import kr.sparta.tripmate.data.dao.CategoryDao
+import kr.sparta.tripmate.data.model.budget.Category
+import kr.sparta.tripmate.data.room.BudgetDatabase
+
+class CategoryLocalDataSource(context: Context) {
+    private val categoryDao: CategoryDao = BudgetDatabase.getDatabsae(context).getCategoryDao()
+
+    suspend fun insertCategories(vararg category: Category) {
+        categoryDao.insertCategories(*category)
+    }
+
+    suspend fun updateCategories(vararg category: Category) {
+        categoryDao.updateCategories(*category)
+    }
+
+    suspend fun deleteCategories(vararg category: Category) {
+        categoryDao.deleteCategories(*category)
+    }
+
+    suspend fun getAllCategoriesWithNum(num: Int): List<Category> =
+        categoryDao.getAllCategoriesWithNum(num)
+
+    suspend fun getAllCategoriesWithBudgetNum(budgetNum: Int): List<Category> =
+        categoryDao.getAllCategoriesWithBudgetNum(budgetNum)
+
+    //flow
+    fun getAllCategoriesToFlowWithBudgetNum(budgetNum: Int): Flow<List<Category>> =
+        categoryDao.getAllCategoriesToFlowWithBudgetNum(budgetNum)
+}
