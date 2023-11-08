7a0b410d992cb6c6ffa39e5a75b160fe3ea1f848
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:45:02 +0900

    [Refactor] BudgetViewModel repository -> usecase 변경

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/BudgetFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/budget/BudgetFactory.kt
@@ -1,4 +1,38 @@
 package kr.sparta.tripmate.ui.viewmodel.budget
 
-class BudgetFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.sparta.tripmate.data.datasource.local.budget.BudgetCategoriesLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.BudgetLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.CategoryLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.CategoryProceduresLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.ProcedureLocalDataSource
+import kr.sparta.tripmate.data.repository.budget.BudgetCategoriesRepositoryImpl
+import kr.sparta.tripmate.data.repository.budget.BudgetRepositoryImpl
+import kr.sparta.tripmate.data.repository.budget.CategoryRepositoryImpl
+import kr.sparta.tripmate.data.repository.budget.ProcedureRepositoryImpl
+import kr.sparta.tripmate.domain.repository.budget.SaveRepository
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBudgetsToFlowWhenBugetsChangedUseCase
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBugetsToFlowWhenProceduresChangedUseCase
+import kr.sparta.tripmate.util.TripMateApp
+
+
+class BudgetFactory : ViewModelProvider.Factory {
+    private val budgetRepository by lazy {
+        BudgetRepositoryImpl(
+            BudgetLocalDataSource(TripMateApp.getApp().applicationContext),
+            ProcedureLocalDataSource(TripMateApp.getApp().applicationContext),
+            CategoryProceduresLocalDataSource(TripMateApp.getApp().applicationContext),
+        )
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BudgetViewModel::class.java)) {
+            return BudgetViewModel(
+                GetAllBudgetsToFlowWhenBugetsChangedUseCase(budgetRepository),
+                GetAllBugetsToFlowWhenProceduresChangedUseCase(budgetRepository),
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
