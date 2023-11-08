83798d25768a63b09512f5ec03eab44a5554c1a9
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:46:31 +0900

    [Refactor] HomeBudgetViewModel repository -> usecase 변경

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/budget/HomeBudgetFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/budget/HomeBudgetFactory.kt
@@ -2,15 +2,29 @@ package kr.sparta.tripmate.ui.viewmodel.home.budget
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.domain.repository.budget.BudgetRepository
-import kr.sparta.tripmate.ui.viewmodel.budget.BudgetViewModel
+import kr.sparta.tripmate.data.datasource.local.budget.BudgetLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.CategoryProceduresLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.ProcedureLocalDataSource
+import kr.sparta.tripmate.data.repository.budget.BudgetRepositoryImpl
+import kr.sparta.tripmate.domain.repository.budget.SaveRepository
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBudgetsToFlowWhenBugetsChangedUseCase
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBugetsToFlowWhenProceduresChangedUseCase
+import kr.sparta.tripmate.util.TripMateApp
 
-class HomeBudgetFactory(
-    private val repository: BudgetRepository,
-) : ViewModelProvider.Factory {
+class HomeBudgetFactory : ViewModelProvider.Factory {
+    private val budgetRepository by lazy {
+        BudgetRepositoryImpl(
+            BudgetLocalDataSource(TripMateApp.getApp().applicationContext),
+            ProcedureLocalDataSource(TripMateApp.getApp().applicationContext),
+            CategoryProceduresLocalDataSource(TripMateApp.getApp().applicationContext),
+        )
+    }
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeBudgetViewModel::class.java)) {
-            return HomeBudgetViewModel(repository) as T
+            return HomeBudgetViewModel(
+                GetAllBudgetsToFlowWhenBugetsChangedUseCase(budgetRepository),
+                GetAllBugetsToFlowWhenProceduresChangedUseCase(budgetRepository),
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class")
     }
--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/budget/HomeBudgetViewModel.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/home/budget/HomeBudgetViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import kr.sparta.tripmate.data.model.budget.Budget
-import kr.sparta.tripmate.domain.repository.budget.BudgetRepository
+import kr.sparta.tripmate.domain.repository.budget.SaveRepository
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBudgetsToFlowWhenBugetsChangedUseCase
+import kr.sparta.tripmate.domain.usecase.budgetrepository.GetAllBugetsToFlowWhenProceduresChangedUseCase
 
-class HomeBudgetViewModel(repository: BudgetRepository) : ViewModel() {
+class HomeBudgetViewModel(
+    getAllBudgetsToFlowWhenBugetsChangedUseCase: GetAllBudgetsToFlowWhenBugetsChangedUseCase,
+    getAllBugetsToFlowWhenProceduresChangedUseCase:GetAllBugetsToFlowWhenProceduresChangedUseCase,
+) : ViewModel() {
     val budgetLiveDataWhenBudgetChanged: LiveData<List<Budget>> =
-        repository.getAllBudgetsToFlowWhenBugetsChanged().asLiveData()
+        getAllBudgetsToFlowWhenBugetsChangedUseCase().asLiveData()
     val budgetLiveDataWhenProcedureChanged: LiveData<List<Budget>> =
-        repository.getAllBugetsToFlowWhenProceduresChanged().asLiveData()
+        getAllBugetsToFlowWhenProceduresChangedUseCase().asLiveData()
 }
