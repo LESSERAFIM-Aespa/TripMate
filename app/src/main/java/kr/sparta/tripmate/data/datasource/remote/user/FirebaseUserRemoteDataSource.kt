62dcb6ed49c4a602ae2d6c9d54e63f3486bc9115
ssu <loveforggong@gmail.com>
2023-11-09 02:01:59 +0900

    [feat] UserRemoteDataSource : (1) 유저 저장 method에 닉네임을 추가로 저장합니다.(닉네임을 저장하는 데이터경로를 추가합니다.) (2) 회원탈퇴 method에서 auth의 유저정보를 추가로 삭제합니다. (3)로그아웃 method를 추가합니다. (4) 따로저장된 닉네임을 불러옵니다. 로그인을 할 때 닉네임 중복체크를 진행하고, 불리언값을 반환해서 사용합니다.

--- app/src/main/java/kr/sparta/tripmate/data/datasource/remote/user/FirebaseUserRemoteDataSource.kt
+++ app/src/main/java/kr/sparta/tripmate/data/datasource/remote/user/FirebaseUserRemoteDataSource.kt
@@ -1,18 +1,15 @@
 package kr.sparta.tripmate.data.datasource.remote.user
 
-import android.content.Context
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.database.ktx.snapshots
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kr.sparta.tripmate.data.model.user.UserData
-import kr.sparta.tripmate.domain.model.user.toEntity
-import kr.sparta.tripmate.util.method.shortToast
-import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
+import kotlin.coroutines.resume
 
 /**
  * 작성자: 서정한
@@ -20,6 +17,10 @@ import kr.sparta.tripmate.util.sharedpreferences.SharedPreferences
  * */
 class FirebaseUserRemoteDataSource {
     private fun getReference() = Firebase.database.getReference("UserData")
+
+    private fun getNickReference() = Firebase.database.getReference("NickNameData")
+    private fun authReference() = FirebaseAuth.getInstance()
+
     /**
      * 작성자 : 서정한
      * 내용 : 유저정보를 가져옵니다.
@@ -36,21 +37,64 @@ class FirebaseUserRemoteDataSource {
      * 내용 : 유저정보를 저장합니다.
      * 자기소개를 수정하고 저장 후 업데이트 합니다.
      * 추후 프로필 사진을 수정하거나 다른 데이터를 수정해도 재사용 가능합니다.
+     *
+     * 추가 : 닉네임을 추가로 저장합니다. (닉네임 중복검사를 통해 닉네임을 누적저장합니다.)
      */
     fun saveUserData(model: UserData) {
-        val ref = model.login_Uid?.let { 
-            getReference().child(it) 
+        val ref = model.login_Uid?.let {
+            getReference().child(it)
         }
-        
+
+        val nickRef = getNickReference()
+        model.login_NickName?.let {
+            nickRef.get().addOnSuccessListener { snapshot ->
+                val nickList = snapshot.children.mapNotNull {
+                    it.getValue(String::class.java)
+                }.toMutableList()
+                nickList.add(model.login_NickName)
+                nickRef.setValue(nickList)
+            }
+        }
         ref?.setValue(model)
     }
 
     /**
      * 작성자 : 서정한
      * 내용 : 회원탈퇴.
+     *
+     * 추가 : RDB에서 삭제 하는 부분에서 auth에서 삭제하는 부분을 추가했습니다.
      */
     fun withdrawalUserData(uid: String) {
+        val user = authReference().currentUser
         val userRef = getReference().child(uid)
+        user?.delete()
         userRef.removeValue()
+    }
+
+    /**
+     * 작성자 : 박성수
+     * 내용 : 로그아웃을 합니다.
+     */
+
+    fun logout() = authReference().signOut()
+
+    /**
+     * 작성자 : 박성수
+     * 내용 : 따로 RDB에 저장된 닉네임 데이터를 불러옵니다.
+     * 불리언값을 이용해 앱을 실행하고 닉네임으 정할때 중복체크를 합니다.
+     */
+    suspend fun getNickNameData(nickName: String): Boolean {
+        val nickRef = getNickReference()
+
+            val result = suspendCancellableCoroutine<Boolean> {continuation ->
+                nickRef.get().addOnSuccessListener { snapshot ->
+                    val isExist = snapshot.children.mapNotNull {
+                        it.getValue(String::class.java)
+                    }.toMutableList().find { it == nickName }.isNullOrEmpty()
+                    continuation.resume(isExist)
+                }
+            }
+
+        return result
     }
 }
