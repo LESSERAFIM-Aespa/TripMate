6ab381256c76413ef395f18038de8155c6d1d5d3
PotatoMeme <kshk0897@gmail.com>
2023-11-08 13:24:47 +0900

    [Refactor] ProcedureRepositoryImpl DI 주입

---
+++ app/src/main/java/kr/sparta/tripmate/data/repository/budget/ProcedureRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package kr.sparta.tripmate.data.repository.budget
+
+import kotlinx.coroutines.flow.Flow
+import kr.sparta.tripmate.data.datasource.local.budget.BudgetCategoriesLocalDataSource
+import kr.sparta.tripmate.data.datasource.local.budget.ProcedureLocalDataSource
+import kr.sparta.tripmate.data.model.budget.Procedure
+import kr.sparta.tripmate.domain.repository.budget.ProcedureRepository
+
+class ProcedureRepositoryImpl(
+    private val procedureDataSource: ProcedureLocalDataSource,
+    private val budgetCategoriesDataSource: BudgetCategoriesLocalDataSource,
+) : ProcedureRepository {
+    override suspend fun insertProcedures(vararg procedures: Procedure) {
+        procedureDataSource.insertProcedures(*procedures)
+    }
+
+    override suspend fun updateProcedures(vararg procedures: Procedure) {
+        procedureDataSource.updateProcedures(*procedures)
+    }
+
+    override suspend fun deleteProcedures(vararg procedures: Procedure) {
+        procedureDataSource.deleteProcedures(*procedures)
+    }
+
+    override suspend fun getAllProceduresWithBudgetNum(num: Int): List<Procedure> =
+        procedureDataSource.getAllProcedures().run {
+            val list = budgetCategoriesDataSource.getAllBudgetCategories(num).map { categoryList ->
+                categoryList.categories.orEmpty().map { it.num }
+            }.reduce { acc, ints -> acc + ints }
+            procedureDataSource.getAllProceduresOrderByTimeWithCategoryNums(list)
+        }
+
+    override suspend fun getProceduresWithNum(num: Int): List<Procedure> =
+        procedureDataSource.getAllProceduresWithNum(num)
+
+    override suspend fun getProcedouresWithCategoryNum(num: Int): List<Procedure> =
+        procedureDataSource.getAllProceduresWithCategoryNum(num)
+
+    override suspend fun getAllProceuduresWithCategoryNums(nums: List<Int>): List<Procedure> =
+        procedureDataSource.getAllProceduresWithCategoryNums(nums)
+
+    override fun getProcedureToFlowWithNum(num: Int): Flow<List<Procedure>> =
+        procedureDataSource.getAllProceduresWithNumToFlow(num)
+}
