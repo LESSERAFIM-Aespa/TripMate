33243e98e4b812ba7b54d68b4e59927f4a641a6d
SeoJungHan <seojh5939@gmail.com>
2023-11-08 15:14:38 +0900

    [Refector] : 유저 Domain영역 리펙토링

--- app/src/main/java/kr/sparta/tripmate/data/repository/user/FirebaseUserRepositoryImpl.kt
+++ app/src/main/java/kr/sparta/tripmate/data/repository/user/FirebaseUserRepositoryImpl.kt
@@ -2,8 +2,12 @@ package kr.sparta.tripmate.data.repository.user
 
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kr.sparta.tripmate.data.datasource.remote.user.FirebaseUserRemoteDataSource
+import kr.sparta.tripmate.data.model.user.UserData
 import kr.sparta.tripmate.domain.model.user.UserDataEntity
+import kr.sparta.tripmate.domain.model.user.toModel
 import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
 
 /**
@@ -12,19 +16,11 @@ import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
  * */
 class FirebaseUserRepositoryImpl(private val remoteSource: FirebaseUserRemoteDataSource) :
     FirebaseUserRepository {
-    override fun updateUserData(uid: String, userLiveData: MutableLiveData<UserDataEntity?>) {
-        remoteSource.updateUserData(uid, userLiveData)
-    }
+    override fun getUserData(uid: String): Flow<UserData> = remoteSource.getUserData(uid)
 
-    override fun saveUserData(
-        model: UserDataEntity,
-        context: Context,
-        userLiveData: MutableLiveData<UserDataEntity?>
-    ) {
-        remoteSource.saveUserData(model, context, userLiveData)
-    }
+    override fun saveUserData(model: UserDataEntity) = remoteSource.saveUserData(model.toModel())
 
-    override fun resignUserData(context: Context) {
-        remoteSource.resignUserData(context)
-    }
+    override fun updateUserData(model: UserDataEntity) = remoteSource.saveUserData(model.toModel())
+
+    override fun withdrawalUserData(uid: String) = remoteSource.withdrawalUserData(uid)
 }
