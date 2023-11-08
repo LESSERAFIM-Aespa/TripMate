eba2ea59720d1ecbc46a8a371773eb187784f5b7
SeoJungHan <seojh5939@gmail.com>
2023-11-08 15:18:48 +0900

    [Refector] : UserProfile ViewModel UseCase 수정

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/userproflie/UserProfileFactory.kt
@@ -2,11 +2,11 @@ package kr.sparta.tripmate.ui.viewmodel.userproflie
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kr.sparta.tripmate.data.datasource.remote.community.FirebaseBoardRemoteDataSource
 import kr.sparta.tripmate.data.datasource.remote.user.FirebaseUserRemoteDataSource
 import kr.sparta.tripmate.data.repository.user.FirebaseUserRepositoryImpl
 import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
-import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.UpdateUserData
+import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.GetUserDataUseCase
+import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.UpdateUserDataUseCase
 
 class UserProfileFactory : ViewModelProvider.Factory{
     private val repository : FirebaseUserRepository by lazy {
@@ -16,7 +16,7 @@ class UserProfileFactory : ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(UserProfileViewModel::class.java)){
             return UserProfileViewModel(
-                UpdateUserData(repository)
+                GetUserDataUseCase(repository)
             ) as T
         }
         throw IllegalArgumentException("에러")
