9ffe56d8d916bc10c8a6025d8466dfdf3df086c7
PotatoMeme <kshk0897@gmail.com>
2023-11-13 11:35:30 +0900

    [Refactor] LoginActivity : SharedPreference object -> UseCase

--- app/src/main/java/kr/sparta/tripmate/ui/viewmodel/login/LoginFactory.kt
+++ app/src/main/java/kr/sparta/tripmate/ui/viewmodel/login/LoginFactory.kt
@@ -2,23 +2,40 @@ package kr.sparta.tripmate.ui.viewmodel.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kr.sparta.tripmate.data.datasource.local.budget.SharedPreferencesLocalDataSource
 import kr.sparta.tripmate.data.datasource.remote.user.FirebaseUserRemoteDataSource
+import kr.sparta.tripmate.data.repository.sharedpreference.SharedPreferenceReopositoryImpl
 import kr.sparta.tripmate.data.repository.user.FirebaseUserRepositoryImpl
+import kr.sparta.tripmate.domain.repository.sharedpreference.SharedPreferenceReopository
 import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
 import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.GetNickNameDataUseCase
-import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.GetUserDataUseCase
 import kr.sparta.tripmate.domain.usecase.firebaseuserrepository.SaveUserDataUseCase
+import kr.sparta.tripmate.domain.usecase.sharedpreference.GetNickNameUseCase
+import kr.sparta.tripmate.domain.usecase.sharedpreference.GetUidUseCase
+import kr.sparta.tripmate.domain.usecase.sharedpreference.SaveNickNameUseCase
+import kr.sparta.tripmate.domain.usecase.sharedpreference.SaveProfileUseCase
+import kr.sparta.tripmate.domain.usecase.sharedpreference.SaveUidUseCase
+import kr.sparta.tripmate.util.TripMateApp
 
 class LoginFactory : ViewModelProvider.Factory {
-    private val repository : FirebaseUserRepository by lazy {
+    private val firebaseUserRepository: FirebaseUserRepository by lazy {
         FirebaseUserRepositoryImpl(FirebaseUserRemoteDataSource())
     }
 
+    private val sharedPreferenceReopository: SharedPreferenceReopository by lazy {
+        SharedPreferenceReopositoryImpl(SharedPreferencesLocalDataSource(TripMateApp.getApp().applicationContext))
+    }
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(LoginViewModel::class.java)){
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
             return LoginViewModel(
-                SaveUserDataUseCase(repository),
-                GetNickNameDataUseCase(repository)
+                SaveUserDataUseCase(firebaseUserRepository),
+                GetNickNameDataUseCase(firebaseUserRepository),
+                SaveUidUseCase(sharedPreferenceReopository),
+                SaveProfileUseCase(sharedPreferenceReopository),
+                SaveNickNameUseCase(sharedPreferenceReopository),
+                GetUidUseCase(sharedPreferenceReopository),
+                GetNickNameUseCase(sharedPreferenceReopository)
             ) as T
         }
         throw IllegalArgumentException("에러")
