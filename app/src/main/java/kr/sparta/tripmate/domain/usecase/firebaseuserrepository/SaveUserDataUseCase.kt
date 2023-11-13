d6948ffcbec9adc965557689b90171e026b63610
PotatoMeme <kshk0897@gmail.com>
2023-11-13 22:19:03 +0900

    [Refactor] Domain 필요없는 함수 삭제

--- app/src/main/java/kr/sparta/tripmate/domain/usecase/firebaseuserrepository/SaveUserDataUseCase.kt
+++ app/src/main/java/kr/sparta/tripmate/domain/usecase/firebaseuserrepository/SaveUserDataUseCase.kt
@@ -1,7 +1,5 @@
 package kr.sparta.tripmate.domain.usecase.firebaseuserrepository
 
-import android.content.Context
-import androidx.lifecycle.MutableLiveData
 import kr.sparta.tripmate.domain.model.user.UserDataEntity
 import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
 
--- app/src/main/java/kr/sparta/tripmate/domain/usecase/firebaseuserrepository/UpdateUserDataUseCase.kt
+++
@@ -1,9 +0,0 @@
-package kr.sparta.tripmate.domain.usecase.firebaseuserrepository
-
-import androidx.lifecycle.MutableLiveData
-import kr.sparta.tripmate.domain.model.user.UserDataEntity
-import kr.sparta.tripmate.domain.repository.user.FirebaseUserRepository
-
-class UpdateUserDataUseCase(private val repository: FirebaseUserRepository) {
-    operator fun invoke(model: UserDataEntity) = repository.saveUserData(model)
-}
