7ca7895c8c2b63b99867fcbe8e5505c8e0bef84a
SeoJungHan <seojh5939@gmail.com>
2023-11-15 14:08:16 +0900

    앱 서명키 변경으로인한 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "kr.sparta.tripmate"
         minSdk = 31
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
