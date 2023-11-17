3ba0520be2f5ffdb6d86252253b5507686eee3ed
ssu <loveforggong@gmail.com>
2023-11-17 09:53:47 +0900

    [feat]1.0.4버전으로 업데이트 되었습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "kr.sparta.tripmate"
         minSdk = 31
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
