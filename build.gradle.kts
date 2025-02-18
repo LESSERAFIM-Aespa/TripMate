acc98a532d0c4ba2a9aa27896cbc2801224b7460
SeoJungHan <seojh5939@gmail.com>
2025-02-18 22:33:14 +0900

    gradle migration to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
-buildscript {
-    buildscript {
-        repositories {
-            mavenCentral()
-        }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
-    }
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.room) apply false
+//    id("com.google.gms.google-services") apply false
+//    id("com.google.firebase.crashlytics") apply false
 }
