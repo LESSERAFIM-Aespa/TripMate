d23777e6d07239a5803e3987cdcdfb2c8b2af957
SeoJungHan <seojh5939@gmail.com>
2026-01-18 17:00:25 +0900

    버전 1.0.7 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "kr.sparta.tripmate"
         minSdk = 27
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.6"
+        versionCode = 10
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
