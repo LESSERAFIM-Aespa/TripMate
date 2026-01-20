a34ca3c7a12445ee3868fae943802af97bfc5799
David.seo <seojh5939@gmail.com>
2026-01-20 18:39:33 +0900

    develop fix : Naver 헤더 주입 방식 정리

--- app/src/main/java/kr/sparta/tripmate/data/datasource/remote/search/SearchBlogRemoteDataSource.kt
+++ app/src/main/java/kr/sparta/tripmate/data/datasource/remote/search/SearchBlogRemoteDataSource.kt
@@ -1,9 +1,7 @@
 package kr.sparta.tripmate.data.datasource.remote.search
 
-import kr.sparta.tripmate.api.Constants
 import kr.sparta.tripmate.remote.model.search.SearchBlogServerData
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 /**
@@ -11,11 +9,6 @@ import retrofit2.http.Query
  * 내용: 블로그 검색
  * */
 interface SearchBlogRemoteDataSource {
-    @Headers(
-        "X-Naver-Client-Id: ${Constants.client_id}",
-        "X-Naver-Client-Secret: ${Constants.client_secret}"
-    )
-
     @GET("search/blog.json")
     suspend fun getScrap(
         @Query("query") query : String,
